fd983f5336ee6b6f66e72b81cd720f440c2a51c7
AngelaPoland <angelalpoland@gmail.com>
2018-05-07 16:52:16 -0700

    adds index and show controller tests -passing- for movies controller

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,14 +1,74 @@
 require "test_helper"
 
 describe MoviesController do
-  it "should get index" do
-    get movies_index_url
-    value(response).must_be :success?
+  describe "Index" do
+    it "is a real working route" do
+      get movies_path
+
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get movies_path
+
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an array" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all movies" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+    it "returns movies with all fields" do
+      keys = %w(inventory overview release_date title)
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal keys
+      end
+    end
   end
 
-  it "should get show" do
-    get movies_show_url
-    value(response).must_be :success?
-  end
+  describe "Show" do
+    it "gets a movie" do
+      get movie_path(movies(:one).id)
 
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get movie_path(movies(:one).id)
+
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns a movie hash" do
+      get movie_path(movies(:one).id)
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+    end
+
+    it "returns a 404 for movie not found" do
+      movie = movies(:one)
+      movie.destroy
+      get movie_path(movie.id)
+
+      must_respond_with :not_found
+    end
+
+
+
+
+  end
 end
