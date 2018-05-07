55a5ab5085e20b2ac58abcb2f88284bbd4547991
Lily Sky <lilyxsky@gmail.com>
2018-05-07 14:04:34 -0700

    created controller for movies and customers, rails reflect controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   #customers_controller
     resources :customers, only:[:index]
 
   #movies_controller
-    # index
-    # show
-    # create
     resources :movies, only:[:index, :show, :create]
 
 end
