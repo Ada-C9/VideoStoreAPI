41168b6b448a918892fd2ddb13e5a7182d4f7891
Hannah Cameron <hannahiscoding@gmail.com>
2018-05-07 16:32:23 -0700

    added and passing customer validation tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,30 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  describe 'validations' do
+    before do
+            @old_customer_count = Customer.count
+    end
+    it 'will not create  have a name' do
+      customer = Customer.new(name: "Honey BooBoo")
+
+      customer.must_be :valid?
+
+      customer.save
+
+      Customer.count.must_equal @old_customer_count + 1
+    end
+
+    it 'is invaild without a name' do
+      customer = Customer.new(name: nil)
+
+      customer.wont_be :valid?
+
+      customer.save
+
+      Customer.count.must_equal @old_customer_count
+    end
   end
+
 end
