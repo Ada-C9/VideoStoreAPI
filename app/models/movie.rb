96c7dcb8c5af09924ed69333e575ee4314e4ef70
Brittany Jones <brittany.r.jones@hotmail.com>
2018-05-07 16:49:01 -0700

    added validations to models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ApplicationRecord
   has_many :rentals
+  validates :title, presence :true
+  validates :release_date, presence :true
+  validates :inventory, presence :true
+
+
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,8 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :movie
+  validates :customer_id, presence :true
+  validates :movie_id, presence :true
+  validates :check_in, presence :true
+  validates :check_out, presence :true
 end
