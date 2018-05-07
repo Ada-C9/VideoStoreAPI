3bc954bb80d9ff71fbc428acaca513764ccde356
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-05-07 15:05:35 -0700

    add validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
 class Customer < ApplicationRecord
+  has_many :rentals
+
+  validates :name, presence: true
+  validates :registered_at, presence: true
+  validates :postal_code, presence:true
+  validates :phone, presence: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :rentals
+  validates :title, presence: true
+  validates :release_date, presence: true
+  validates :inventory, presence: true
 end
