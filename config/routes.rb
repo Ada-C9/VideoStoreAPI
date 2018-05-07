508928abcc029cafdd3effe137dff63e4ca97cb1
kseastman <kat.eastman@gmail.com>
2018-05-07 15:07:44 -0700

    added routes and rentals controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  post 'rentals/checkout', to: 'rentals#checkout', as: :checkout
+
+  post 'rentals/checkin', to: 'rentals#checkin', as: :checkin
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/zomg', to: 'movies#zomg'
+  resources :customers, only: [:index]
+  resources :movies, only: [:index, :show]
 end
