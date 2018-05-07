2519d2c305dca903029d12ed43099e878cc9cdb9
Brenda Rios <riosbrenda@gmail.com>
2018-05-07 16:35:47 -0700

    Tests for customers index passing.

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe 'index'do
+  it "is a real working route" do
+    get customers_url
+    must_respond_with :success
+  end
+
+  it "returns json" do
+    get customers_url
+    response.header['Content-Type'].must_include 'json'
+  end
+
+  it "returns an Array" do
+    get customers_url
+
+    body = JSON.parse(response.body)
+    body.must_be_kind_of Array
+  end
+
+  it "returns all of the customers" do
+    get customers_path
+
+    body = JSON.parse(response.body)
+    body.length.must_equal Customer.count
+  end
+
+  it "returns pets with exactly the required fields" do
+    keys = %w(id name phone postal_code registered_at)
+    get customers_url
+    body = JSON.parse(response.body)
+    body.each do |costumer|
+      costumer.keys.sort.must_equal keys
+    end
+  end
+end
+
 end
