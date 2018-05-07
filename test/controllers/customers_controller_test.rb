47b028abd78ac99c919820f193605f3d8c7c806c
Lily Sky <lilyxsky@gmail.com>
2018-05-07 16:18:58 -0700

    customer - controller tests, controller index, fixutres, model tests start

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "is a real working route" do
+      get customers_url
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get customers_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array" do
+      get customers_url
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the customers" do
+      get customers_url
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+
+    it "customers must have names and phone nums" do
+      keys = %w(address city id name phone postal_code registered_at)
+      get customers_url
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal keys.sort
+      end
+    end
+
+  end
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,14 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
-
   it "must be valid" do
-    value(customer).must_be :valid?
+    value(customers(:one)).must_be :valid?
+  end
+
+  it "must have a name" do
+    customer = customers(:one)
+    customer.name = nil
+
+    customer.valid?.must_be false
   end
 end
