bea5ffa4561ebf7a0fcb9fad3117b4073d256a6b
AngelaPoland <angelalpoland@gmail.com>
2018-05-07 16:06:33 -0700

    adds index and show actions to rental controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,8 +1,17 @@
 class RentalsController < ApplicationController
   def index
+    rentals = Rental.all
+    render json: rentals.as_json(only: [:customer_id, :movie_id, :checkout_date])
   end
 
   def show
+    rental = Rental.find_by(id: params[:id])
+
+    if rental
+      render json: rental.as_json(only: [:customer_id, :movie_id, :checkout_date]), status: :ok
+    else
+      render json: {ok: false, errors: "Rental not found"}, status: :not_found
+    end
   end
 
   def create
