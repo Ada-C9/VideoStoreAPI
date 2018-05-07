604e3dfa93c5636198baa33630525e2bf8258a0e
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-05-07 14:38:23 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-
-  get 'movies/show'
-
-  get 'movies/create'
-
-  get 'customers/index'
-
+  resources :movies, only: [:index, :show, :create]
+  resources :customers, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
