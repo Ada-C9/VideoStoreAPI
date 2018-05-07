083604deecf17ff75454d373d405e24581e6440e
Aruna <aruna.thungathurthi@gmail.com>
2018-05-07 15:18:55 -0700

    added customer model tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -2,8 +2,22 @@ require "test_helper"
 
 describe Customer do
   let(:customer) { Customer.new }
+  let(:customer) { customers(:Mary)}
+
+  describe "relations" do
+    it "has a list of rentals" do
+      mary = customers(:mary)
+      mary.must_respond_to :rentals
+      mary.rentals.each do |rental|
+        mary.must_be_kind_of Rental
+      end
+    end
+
+  end
 
   it "must be valid" do
-    value(customer).must_be :valid?
+
+
+
   end
 end
