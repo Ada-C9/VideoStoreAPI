f27dd84a4a3257ea8dce557e396e49238b0e2f2d
Aruna <aruna.thungathurthi@gmail.com>
2018-05-07 16:07:18 -0700

    customer model tests passing

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -2,14 +2,16 @@ require "test_helper"
 
 describe Customer do
   let(:customer) { Customer.new }
-  let(:customer) { customers(:Mary)}
+  let(:customer) { customers(:mary)}
 
   describe "relations" do
     it "has a list of rentals" do
       mary = customers(:mary)
-      mary.must_respond_to :rentals
-      mary.rentals.each do |rental|
-        mary.must_be_kind_of Rental
+
+      customer.rentals.each do |rental|
+        rental.must_be_kind_of Rental
+        rental.customer.must_be_instance_of Customer
+        rental.customer.must_equal mary
       end
     end
 
