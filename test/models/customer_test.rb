3766bf4745b5a6b4c3b3d37ec223065ee9a6fbcf
Dikla <dikla.aharoni@gmail.com>
2018-05-07 16:46:25 -0700

    Added validation for customer name and validation tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -2,23 +2,64 @@ require "test_helper"
 
 describe Customer do
   describe "validations" do
-
-  end
-
-  describe "relations" do
+    # all validations pass
     before do
+      customer = Customer.first
+
+
       @customer = Customer.new(
         name: "Movie Watcher",
         address: "2015-04-29T14:54:14.000Z",
         city: "Seattle",
         state: "WA",
         postal_code: "98109",
-        phone: "555-555-5555"
-      )
+        phone: "555-555-5555")
+      end
+
+      it "can be created will all required fields" do
+        # Act
+        result = @customer.valid?
+
+        # Assert
+        result.must_equal true
+
+      end
+
+      it "is invalid without a name" do
+        @customer.name = nil
+
+        result = @customer.valid?
+
+        result.must_equal false
+        @customer.errors.messages.must_include :name
+      end
+
+      # duplicate title -> fail
+      # it "is invalid with a duplicate title" do
+      #   dup_customer = Customer.first
+      #   @customer.title = dup_customer.title
+      #
+      #   result = @customer.valid?
+      #
+      #   result.must_equal false
+      #   @customer.errors.messages.must_include :title
+      # end
     end
 
-    it "has movies" do
-      
+    describe "relations" do
+      before do
+        @customer = Customer.new(
+          name: "Movie Watcher",
+          address: "2015-04-29T14:54:14.000Z",
+          city: "Seattle",
+          state: "WA",
+          postal_code: "98109",
+          phone: "555-555-5555"
+        )
+      end
+
+      it "has movies" do
+
+      end
     end
   end
-end
