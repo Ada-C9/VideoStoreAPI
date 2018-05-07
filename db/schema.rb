01aafa48b22d12c73bb550f111521bc307473cd5
0velez0 <mariaivelez@gmail.com>
2018-05-07 15:38:38 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180507223454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "overview"
+    t.date "release_date"
+    t.integer "inventory"
+    t.integer "available_inventory"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
