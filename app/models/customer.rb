58e61301acc93ecfc293a139733bb109bd8cb89f
Wenjie <traxiaojie@gmail.com>
2018-05-07 14:10:27 -0700

    Added model validations Implemented tests for customer model validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,13 @@
 class Customer < ApplicationRecord
+  validates :name, presence: true
+  validates :phone, presence: true
+  validate :same_name_and_same_phone
+
+  def same_name_and_same_phone
+    customers = Customer.where(name: name)
+
+    if customers.any? {|customer| customer.phone == phone}
+      errors[:phone] << "Can not have the same name and same phone number"
+    end
+  end
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,49 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  describe "validations" do
+    before do
+      @customer = Customer.new(
+        name: "coco",
+        address: "114 Pine St.",
+        city: "Seattle",
+        state: "Washington",
+        postal_code: "98100",
+        phone: "(206) 206 2066"
+      )
+    end
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+    it "can be created with sufficient data" do
+      result = @customer.valid?
+      result.must_equal true
+    end
+
+    it "can not be created without a name" do
+      @customer.name = ""
+      result = @customer.valid?
+      result.must_equal false
+    end
+
+    it "can not be created without a phone number" do
+      @customer.phone = ""
+      result = @customer.valid?
+      result.must_equal false
+    end
+
+    it "can not be created if with same name and same phone as existed customer" do
+      customer1 = Customer.new(
+        name: "coco",
+        address: "1232314 Pine St.",
+        city: "Portland",
+        state: "Oregon",
+        postal_code: "98100",
+        phone: "(206) 206 2066"
+      )
+
+      @customer.save
+      result = customer1.valid?
+      result.must_equal false
+      customer1.errors.messages.must_include :phone
+    end
   end
 end
