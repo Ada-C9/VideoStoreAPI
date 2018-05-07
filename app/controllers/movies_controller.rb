8716b21da80352596e961f9e93795ad7cbd8a321
AngelaPoland <angelalpoland@gmail.com>
2018-05-07 15:52:56 -0700

    adds index and show actions to movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,16 @@
 class MoviesController < ApplicationController
   def index
+    movies = Movie.all
+    render json: movies.as_json(only: [:title, :release_date, :inventory, :overview])
   end
 
   def show
+    movie = Movie.find_by(id: params[:id])
+
+    if movie
+      render json: movie.as_json(only: [:title, :release_date, :inventory, :overview]), status: :ok
+    else
+      render json: {ok: false, errors: "Movie not found"}, status: :not_found
+    end
   end
 end
