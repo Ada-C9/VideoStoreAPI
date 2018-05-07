3ff55aacc7520c8e98b35645defd197f4d8dfc63
Dikla <dikla.aharoni@gmail.com>
2018-05-07 13:59:20 -0700

    Added the zomg method and route

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,8 +5,12 @@ class CustomersController < ApplicationController
     render json: @customers.as_json(only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone]), status: :ok
   end
 
-  def zomg
-    render json: {"it works"}
-  end
+  # def zomg
+  #   render json: {
+  #     "customer": {
+  #       "name": "it works"
+  #     }
+  #   }
+  # end
 
 end
