acafe5f4fca0ddfa62c92b22ba3a90506eb37a82
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-05-07 16:55:01 -0700

    Model validation testing for Movie

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,40 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  before do
+    @movie_data = {
+      title: 'test title',
+      inventory: 2
+    }
+  end
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  it 'is valid with title and an inventory greater then 1' do
+    movie = Movie.new(@movie_data)
+    movie.must_be :valid?
+  end
+
+  it 'is invalid with no name' do
+    movie = Movie.new(@movie_data)
+    movie.title = ""
+
+    movie.wont_be :valid?
+    movie.errors.messages.must_include :title
+  end
+
+  #REVIEW: Consider test case for a movie with 0 inventory
+  it 'is invalid with an inventory < 0' do
+    movie = Movie.new(@movie_data)
+    movie.inventory = -1
+
+    movie.wont_be :valid?
+    movie.errors.messages.must_include :inventory
+  end
+
+  it 'is invalid with a non-numerical inventory' do
+    movie = Movie.new(@movie_data)
+    movie.inventory = "Aaaa"
+
+    movie.wont_be :valid?
+    movie.errors.messages.must_include :inventory
   end
 end
