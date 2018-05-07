ac4c57552f2b6caa9d42525ebd9adef6696709eb
AngelaPoland <angelalpoland@gmail.com>
2018-05-07 15:47:47 -0700

    adds routes and controllers for customers, movies, and rentals

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :customers, only: [:index, :show]
+  resources :movies, only: [:index, :show]
+  resources :rentals, only: [:index, :show, :create]
 end
