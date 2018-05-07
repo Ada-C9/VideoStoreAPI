3de9c7fa42cca3f099cf32928670e7cc1a5f2798
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-05-07 16:07:36 -0700

    Started validations for rental model and added validated timeliness gem to Gemfile but did not install

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,10 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :movie
+
+  validates :movie_id, presence: true
+  validates :customer_id, presence: true
+  validates :check_out, presence: true
+  validates :due_date, presence: true
+  validates :check_in, numericality: greater_than
 end
