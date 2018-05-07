b1abb0e09035bf791542c1ee566c85c090aa2e14
Alexandria Brown <axbn2190@gmail.com>
2018-05-07 16:58:55 -0700

    rentals controller basic tests index show

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,14 +2,14 @@ class RentalsController < ApplicationController
 
   def index
     rentals = Rental.all
-    render json: rentals.as_json(only: [:customer_id, :movie_id, :checkout_date])
+    render json: rentals.as_json(only: [:customer_id, :movie_id, :checkin_date, :checkout_date])
   end
 
   def show
     rental = Rental.find_by(id: params[:id])
 
     if rental
-      render json: rental.as_json(only: [:customer_id, :movie_id, :checkout_date]), status: :ok
+      render json: rental.as_json(only: [:customer_id, :movie_id, :checkout_date, :checkin]), status: :ok
     else
       render json: {ok: false, errors: "Rental not found"}, status: :not_found
     end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,19 +1,70 @@
 require "test_helper"
 
 describe RentalsController do
-  it "should get index" do
-    get rentals_index_url
-    value(response).must_be :success?
+  describe "Index" do
+    it "is a real working route" do
+      get rentals_path
+
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get rentals_path
+
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an array" do
+      get rentals_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all rentals" do
+      get rentals_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Rental.count
+    end
+
+    it "returns rentals with all fields" do
+      keys = %w(checkin_date checkout_date customer_id movie_id)
+      get rentals_path
+
+      body = JSON.parse(response.body)
+      body.each do |rental|
+        rental.keys.sort.must_equal keys
+      end
+    end
   end
 
-  it "should get show" do
-    get rentals_show_url
-    value(response).must_be :success?
-  end
+  describe "Show" do
+    it "gets a rental" do
+      get rental_path(rentals(:one).id)
 
-  it "should get create" do
-    get rentals_create_url
-    value(response).must_be :success?
-  end
+      must_respond_with :success
+    end
 
+    it "returns json" do
+      get rental_path(rentals(:one).id)
+
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns a rental hash" do
+      get rental_path(rentals(:one).id)
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+    end
+
+    it "returns a 404 for rental not found" do
+      rental = rentals(:one)
+      rental.destroy
+      get rental_path(rental.id)
+
+      must_respond_with :not_found
+    end
+  end
 end
