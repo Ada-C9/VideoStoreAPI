9a0fa37487573cbf2bb30d735cc599c27c1536a5
Dikla <dikla.aharoni@gmail.com>
2018-05-07 13:53:00 -0700

    Added index route and index method in customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,12 @@
 class CustomersController < ApplicationController
+
+  def index
+    customers = Customers.all
+    render json: @customers.as_json(only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone]), status: :ok
+  end
+
+  def zomg
+    render json: {"it works"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :customerss, only: [:index]
+
+  get '/zomg', to: "customers#zomg", as: 'zomg'
 end
