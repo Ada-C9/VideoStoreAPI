7062f43281ad62b09d67f877eb314e172207ec7a
Dikla <dikla.aharoni@gmail.com>
2018-05-07 15:05:28 -0700

    Added index method to movies controoler and created a route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,8 @@
 class MoviesController < ApplicationController
+
+  def index
+    movies = Movie.all
+    render json: customers.as_json(only: [:title, :overview, :release_date, :inventory]), status: :ok
+  end
+
 end
