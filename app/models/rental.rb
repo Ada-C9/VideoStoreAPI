64b062df5a1f01f1d40131665ee4731b6d6ca2fb
AngelaPoland <angelalpoland@gmail.com>
2018-05-07 14:50:06 -0700

    changed rental validation of checkout_date to a date instead of a DateTime

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,8 +3,8 @@ class Rental < ApplicationRecord
   belongs_to :movie
 
   validates :checkout_date, presence: true
-  validate :checkout_date_is_valid_datetime
-  def checkout_date_is_valid_datetime
-    errors.add(:checkout_date, 'must be a valid datetime') if ((DateTime.parse(checkout_date) rescue ArgumentError) == ArgumentError)
+  validate :checkout_date_is_valid_date
+  def checkout_date_is_valid_date
+    errors.add(:checkout_date, 'must be a valid datetime') if ((Date.parse(checkout_date) rescue ArgumentError) == ArgumentError)
   end
 end
