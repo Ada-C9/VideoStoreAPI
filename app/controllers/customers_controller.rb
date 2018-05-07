5e3415d7c42c911f00d847df1d2d7a563445bea2
Cara <cararbrennan@gmail.com>
2018-05-07 16:29:54 -0700

    initial controller methods

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,21 @@
 class CustomersController < ApplicationController
+
+  def index
+    customers = Customer.all
+      render(json: customers.as_json(only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone]), status: :ok)
+  end
+
+  def show
+    customer = Customer.find_by(id: params[:id])
+    if movie.nil?
+      render json: {
+        errors: {
+          id: ["No customer with ID #{params[:id]}"]
+        }
+      }, status: :not_found
+    else
+      render json: customer.as_json(only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone], status: :ok)
+    end
+  end
+
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,23 @@
 class MoviesController < ApplicationController
+  def index
+    movies = Movie.all
+      render(json: movies.as_json(only: [:title, :overview, :inventory]), status: :ok)
+  end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+    if movie.nil?
+      render json: {
+        errors: {
+          id: ["No movie with ID #{params[:id]}"]
+        }
+      }, status: :not_found
+    else
+      render json: movie.as_json(only: [:title, :overview, :inventory], status: :ok)
+    end
+  end
+
+  def create
+    
+  end
 end
