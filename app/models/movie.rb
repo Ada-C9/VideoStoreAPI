4c42e2fe6df163a401aef12f1ab8991400c65791
AngelaPoland <angelalpoland@gmail.com>
2018-05-07 13:53:13 -0700

    adds relationships and validations to models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :rentals
+
+  validates :title, presence: true 
+  validates_numericality_of :inventory, presence: true, greater_than_or_equal_to: 0
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,10 @@
 class Rental < ApplicationRecord
+  belongs_to :customer
+  belongs_to :movie
+
+  validates :checkout_date, presence: true
+  validate :checkout_date_is_valid_datetime
+  def checkout_date_is_valid_datetime
+    errors.add(:checkout_date, 'must be a valid datetime') if ((DateTime.parse(checkout_date) rescue ArgumentError) == ArgumentError)
+  end
 end
