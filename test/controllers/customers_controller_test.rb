62f916385305fb14c3583b6c6cc7022a67576b3f
Alexandria Brown <axbn2190@gmail.com>
2018-05-07 16:37:53 -0700

    customers controller testing

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,15 +1,42 @@
 require "test_helper"
 
 describe CustomersController do
-  it "should get index" do
-    get customers_path
+  describe "Index" do
+    it "is a real working route" do
+      get customers_path
 
-    must_respond_with :success?
-  end
+      must_respond_with :success
+    end
 
-  it "should get show" do
-    get customers_show_url
-    value(response).must_be :success?
+    it "returns json" do
+      get customers_path
+
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an array" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all customers" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+
+    it "returns customers with all fields" do
+      keys = %w(address city id name phone postal_code registered_at state)
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal keys
+      end
+    end
   end
 
 end
