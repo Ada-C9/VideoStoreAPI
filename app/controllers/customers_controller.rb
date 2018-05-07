b800b31df63889f58c94232599f552a1e2c2405e
Alexandria Brown <axbn2190@gmail.com>
2018-05-07 15:57:12 -0700

    index and show methods for customers controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,18 @@
 class CustomersController < ApplicationController
+
   def index
+    customers = Customer.all
+    render json: customers.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone])
   end
 
   def show
+    customer = Customer.find_by(id: params[:id])
+
+    if customer
+      render json: customer.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone]), status: :ok
+    else
+      render json: { ok: false, errors: "Customer not found" }, status: :not_found
+    end
   end
+
 end
