09105d393151ebac306e52c3796b415950a07400
Mary Lamkin <mmlamkin@gmail.com>
2018-05-07 13:42:38 -0700

    modifies movies cont test

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,19 +1,43 @@
 require "test_helper"
 
 describe MoviesController do
-  it "should get index" do
-    get movies_index_url
-    value(response).must_be :success?
+  describe "index" do
+    # These tests are a little verbose - yours do not need to be
+    # this explicit.
+    it "is a real working route" do
+      get movies_url
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get movies_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array" do
+      get movies_url
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the movies" do
+      get movies_url
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+    it "returns movies with exactly the required fields" do
+      keys = %w(release_date title)
+      
+      get movies_url
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal keys
+      end
+    end
   end
 
-  it "should get show" do
-    get movies_show_url
-    value(response).must_be :success?
-  end
-
-  it "should get create" do
-    get movies_create_url
-    value(response).must_be :success?
-  end
 
 end
