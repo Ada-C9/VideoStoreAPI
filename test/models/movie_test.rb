f9eae11a70f59c91d2d5b0b816aca6e0860e14d1
Dikla <dikla.aharoni@gmail.com>
2018-05-07 16:46:04 -0700

    Added validation for movie title and validation tests

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,50 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  describe "validations" do
+    # all validations pass
+    before do
+      # Arrange
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+
+      @movie = Movie.new(
+        title: "test movie",
+        overview: "The unexciting life of a boy",
+        release_date: "1979-01-18",
+        inventory: "10"
+      )
+      end
+
+      it "can be created will all required fields" do
+        # Act
+        result = @movie.valid?
+
+        # Assert
+        result.must_equal true
+
+
+      end
+
+      # no title -> fail
+      it "is invalid without a title" do
+        @movie.title = nil
+
+        result = @movie.valid?
+
+        result.must_equal false
+        @movie.errors.messages.must_include :title
+      end
+
+      # duplicate title -> fail
+      it "is invalid with a duplicate title" do
+        dup_movie = Movie.first
+        @movie.title = dup_movie.title
+
+        result = @movie.valid?
+
+        result.must_equal false
+        @movie.errors.messages.must_include :title
+      end
+    end
+
   end
-end
