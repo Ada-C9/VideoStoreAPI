e3554a21afbc7c80abbdac188cd2de41dafda4ba
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-07 15:06:35 -0700

    Test customer validations

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,20 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  describe "validations" do
+    it "must have a name" do
+      customer = Customer.new(address: "123 Main St.", city: "Seattle", state: "Washington", postal_code: "12345", phone: "777-777-7777", registered_at: DateTime.now)
+      customer.must_be_instance_of Customer
+      customer.wont_be :valid?
+    end
+
+    it "is valid with a name" do
+      customer = Customer.new(name: "Sebastián Piñera")
+      customer.must_be_instance_of Customer
+      customer.must_be :valid?
+    end
+
   end
+
 end
