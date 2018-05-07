c4a833ccf5664831917697a6a54dbe4c6e3f70a3
Mary Lamkin <mmlamkin@gmail.com>
2018-05-07 13:38:59 -0700

    adds customer index controller test DIDN'T DO MOVIES CHECKOUT COUNT

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,7 @@
 class CustomersController < ApplicationController
   def index
+    @customers = Customer.all
+
+    render json: @customers.as_json(only: [:name, :phone, :postal_code, :registered_at])
   end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,9 +1,38 @@
 require "test_helper"
 
 describe CustomersController do
-  it "should get index" do
-    get customers_index_url
-    value(response).must_be :success?
-  end
+  describe "index" do
+    it "is a real working route" do
+      get customers_url
+      must_respond_with :success
+    end
 
+    it "returns json" do
+      get customers_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array" do
+      get customers_url
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the customers" do
+      get customers_url
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+
+    it "returns customers with exactly the required fields" do
+      keys = %w(name phone postal_code registered_at)
+      get customers_url
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal keys
+      end
+    end
+  end
 end
