64eaa54fc75454e5894d07a91da0b0c4febc5410
Tor Shimizu <torshimizu@gmail.com>
2018-05-07 13:56:51 -0700

    wrote movie model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :inventory, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validate :uniq_title_release_date_combo
+
+  def uniq_title_release_date_combo
+    movies = Movie.where(title: title)
+
+    if movies.any?{ |movie| movie.release_date == release_date }
+      errors[:release_date] << 'Cannot have a movie with the same title and release date'
+    end
+  end
 end
