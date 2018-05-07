0acfd34b56fc3c30174212e1425d6001b7b3852c
Anne <annehwatson@gmail.com>
2018-05-07 15:31:41 -0700

    stubs for customer test

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,24 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  describe "validations" do
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  end
+
+  describe "relations" do
+    before do
+      @customer = Customer.new(
+        name: "Movie Watcher",
+        address: "2015-04-29T14:54:14.000Z",
+        city: "Seattle",
+        state: "WA",
+        postal_code: "98109",
+        phone: "555-555-5555"
+      )
+    end
+
+    it "has movies" do
+      
+    end
   end
 end
