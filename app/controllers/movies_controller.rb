b81e1d90d0893bf3da8ffcb0489e5f74d5372737
Dikla <dikla.aharoni@gmail.com>
2018-05-07 15:41:24 -0700

    Added route for movie show and the show method for movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,17 @@ class MoviesController < ApplicationController
     render json: movies.as_json(only: [:title, :overview, :release_date, :inventory]), status: :ok
   end
 
+  def show
+    movie = Movie.find_by(id: params[:id])
+    if movie.nil?
+      render json: {
+        errors: {
+          id: ["No movie with ID #{params[:id]}"]
+        }
+        }, status: :not_found
+      else
+        render json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory]), status: :ok
+      end
+  end
+
 end
