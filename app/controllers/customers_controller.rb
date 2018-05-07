1e48b7578cf96efaa9a573aa9656e144dd2446ee
Wenjie <traxiaojie@gmail.com>
2018-05-07 15:08:50 -0700

    Added movies controller actions Implemented tests for controller movies, all passed

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,6 @@ class CustomersController < ApplicationController
 
   def index
     customers = Customer.all
-    render json: customers.as_json(except: [:created_at, :updated_at] )
+    render json: customers.as_json(except: [:created_at, :updated_at], status: :ok)
   end
 end
