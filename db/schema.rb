2a5406cef08e0c10a90243a9de34276aa96e0b3b
knockknockhusthere <angela.huxia@gmail.com>
2018-05-07 15:59:43 -0700

    added registered date and seeded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180507224416) do
+ActiveRecord::Schema.define(version: 20180507225557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180507224416) do
     t.integer "movies_checked_out_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "registered_at"
   end
 
   create_table "movies", force: :cascade do |t|
