cbc6002faf43b15d777d2a4ad4d7d84396c63787
Hannah Cameron <hannahiscoding@gmail.com>
2018-05-07 15:55:15 -0700

    added model validations to customer and movie

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
   has_many :rentals
   has_many :movies, through: :rentals
+
+  validates :name, presence: true
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
+
+  validates :title, presence: true
+  validates :inventory, presence: true, numericality: { only_integer: true, greater_than: 1 }
+  validates :available_inventory, presence: true, numericality: { only_integer: true }
 end
