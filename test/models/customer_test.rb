8ff62f7355d356e5b18368a1eb2e3dff72df2f07
Lily Sky <lilyxsky@gmail.com>
2018-05-07 16:35:04 -0700

    customer model tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,14 +1,24 @@
 require "test_helper"
 
 describe Customer do
-  it "must be valid" do
-    value(customers(:one)).must_be :valid?
+  describe "validations" do
+    it "must be valid" do
+      value(customers(:one)).must_be :valid?
+    end
+
+    it "must have a name" do
+      customer = customers(:one)
+      customer.name = nil
+
+      customer.valid?.must_equal false
+    end
+
+    it "must have a phone num" do
+      customer = customers(:two)
+      customer.phone = nil
+
+      customer.valid?.must_equal false
+    end
   end
 
-  it "must have a name" do
-    customer = customers(:one)
-    customer.name = nil
-
-    customer.valid?.must_be false
-  end
 end
