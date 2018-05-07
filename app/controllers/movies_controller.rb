b6021a43e14ded853124db84ada8da38c111e692
Brenda Rios <riosbrenda@gmail.com>
2018-05-07 15:19:21 -0700

    Added index method to customers and movies controllers.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,9 @@
 class MoviesController < ApplicationController
 
   def index
+    movies = Movie.all
+    # render :json => movies
+    render(json: movies.as_json(only: [:id, :title, :release_date]), status: :ok)
   end
 
   def show
