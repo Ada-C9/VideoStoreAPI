9f0af194efe6c99a28e28f7b5971d930063100c1
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-05-08 12:59:45 -0700

    update routes to include rentals

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
 
-  get '/customers', to: 'customers#index', as: 'customers'
-
-  get '/movies', to: 'movies#index', as: 'movies'
-
-  post '/movies', to: 'movies#create'
-
-  get '/movies/:id', to: 'movies#show', as: 'movie'
-
+  resources :movies, only: [:index, :show, :create]
+  resources :customers, only: [:index]
+  post 'rentals/checkout', to: 'rentals#checkout', as: 'checkout'
+  post 'rentals/checkin', to: 'rentals#checkin', as: 'checkin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
