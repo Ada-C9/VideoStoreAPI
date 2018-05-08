99bbc92c562d3660adda21b951005c53c7dd30fb
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-05-08 16:47:24 -0700

    added tests for controller movie create and created create method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,8 @@
 class MoviesController < ApplicationController
 
   def index
-  @movies = Movie.where(title: params[:title])
-  render 'movies/index'
+    @movies = Movie.where(title: params[:title])
+    render 'movies/index'
   end
 
   def show
@@ -14,7 +14,24 @@ class MoviesController < ApplicationController
         errors: {
           id: ["No movie with ID #{params[:id]}"]
         }
-      }, status: :not_found
+        }, status: :not_found
+      end
     end
+
+    def create
+      movie = Movie.new(movie_params)
+
+      if movie.save
+        render json: {id: movie.id}, status: :ok
+      else
+        render json: {errors: movie.errors.messages}, status: :bad_request
+      end
+
+    end
+
+    private
+    def movie_params
+      return params.require(:movie).permit(:title, :overwiew, :inventory, :release_date)
+    end
+
   end
-end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,59 +1,102 @@
 require "test_helper"
 
 describe MoviesController do
-    describe "index" do
-      it "returns an array of json" do
-        get movies_url
-        must_respond_with :success
-        response.header['Content-Type'].must_include 'json'
-        body = JSON.parse(response.body)
-        body.must_be_kind_of Array
+  describe "index" do
+    it "returns an array of json" do
+      get movies_url
+      must_respond_with :success
+      response.header['Content-Type'].must_include 'json'
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns movies with a name that matches the search" do
+      get movies_url, params: {search: "Bob Esponja"}
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie["name"].must_equal "Bob Esponja"
       end
+    end
 
-      it "returns movies with a name that matches the search" do
-        get movies_url, params: {search: "Bob Esponja"}
-        body = JSON.parse(response.body)
-        body.each do |movie|
-          movie["name"].must_equal "Bob Esponja"
-        end
+    it "returns movies with exactly the required fields" do
+      keys = %w(available_inventory id inventory overview release_date title)
+      get movies_url, params: {search: "Bob Esponja"}
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal keys
       end
+    end
 
-      it "returns movies with exactly the required fields" do
-        keys = %w(available_inventory id inventory overview release_date title)
-        get movies_url, params: {search: "Bob Esponja"}
-        body = JSON.parse(response.body)
-        body.each do |movie|
-          movie.keys.sort.must_equal keys
-        end
-      end
+  end # index
 
-    end # index
+  describe "show" do
+    it "can get a movie" do
+      keys = %w(available_inventory id inventory overview release_date title )
+      movie = Movie.first
+      get movie_path(movie.id)
+      must_respond_with :success
 
-    describe "show" do
-      it "can get a movie" do
-        keys = %w(available_inventory id inventory overview release_date title )
-        movie = Movie.first
-        get movie_path(movie.id)
-        must_respond_with :success
+      response.header['Content-Type'].must_include 'json'
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.keys.sort.must_equal keys
+      body['id'].must_equal movie.id
+    end
 
-        response.header['Content-Type'].must_include 'json'
-        body = JSON.parse(response.body)
-        body.must_be_kind_of Hash
-        body.keys.sort.must_equal keys
-        body['id'].must_equal movie.id
-      end
+    it "it should return not found and returns some error test when movie does not exist" do
 
-      it "it should return not found and returns some error test when movie does not exist" do
+      movie_id = Movie.last.id + 1
+      get movie_path(movie_id)
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "errors"
+      body["errors"].must_include "id"
 
-        movie_id = Movie.last.id + 1
-        get movie_path(movie_id)
-        must_respond_with :not_found
-        body = JSON.parse(response.body)
-        body.must_be_kind_of Hash
-        body.must_include "errors"
-        body["errors"].must_include "id"
+    end
 
-      end
+  end # show
+  describe "create" do
+    let(:movie_data) {
+      {
+        title: "Isle of Dogs",
+        overview: "great movie",
+        inventory: 3,
+        release_date: Date.today
+      }
+    }
 
-    end # show
+    it "should create a new valid movie" do
+      old_movie_count = Movie.count
+      post movies_url, params: { movie: movie_data }
+      Movie.count.must_equal old_movie_count + 1
+      newest_movie = Movie.last
+      newest_movie.title.must_equal movie_data[:title]
+    end
+
+    it "should yield an error and error text when invalid title for movie" do
+      movie_data[:title] = nil
+      old_movie_count = Movie.count
+      post movies_url, params: { movie: movie_data }
+      Movie.count.must_equal old_movie_count
+      must_respond_with :bad_request
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "errors"
+      body["errors"].must_include "title"
+    end
+
+    it "should yield an error and error text when invalid inventory for movie" do
+      movie_data[:inventory] = nil
+      old_movie_count = Movie.count
+      post movies_url, params: { movie: movie_data }
+      Movie.count.must_equal old_movie_count
+      must_respond_with :bad_request
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "errors"
+      body["errors"].must_include "inventory"
+    end
+  end
+
 end
