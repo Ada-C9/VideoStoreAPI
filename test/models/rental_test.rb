eac8b4bd50bdb780c7b2ddec6c30337af17eb639
Hannah Cameron <hannahiscoding@gmail.com>
2018-05-07 20:34:50 -0700

    wrote relations tests for rentals model

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,9 +1,27 @@
 require "test_helper"
 
 describe Rental do
-  let(:rental) { Rental.new }
+  describe 'relations' do
 
-  it "must be valid" do
-    value(rental).must_be :valid?
+    before do
+      @rental = Rental.new
+    end
+
+    it 'belongs to a movie' do
+
+      movie = Movie.first
+      @rental.movie = movie
+      @rental.movie_id.must_equal movie.id
+
+    end
+
+    it 'belongs to a customer' do
+
+      customer = Customer.first
+      @rental.customer = customer
+      @rental.customer_id.must_equal customer.id
+
+    end
+
   end
 end
