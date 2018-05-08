7cf9f9ba0759b61dc8bce51cf9d5cdb01df1996a
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-05-08 16:48:32 -0700

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :movies, only: [:index, :show, :create]
   resources :customers, only: [:index, :show]
 
+  POST 'rentals/checkout', to: 'rentals#checkout', as: 'checkout'
+  POST 'rentals/checkin', to: 'rentals#checkin', as: 'checkin'
+
 end
