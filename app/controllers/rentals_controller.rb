2bf87a47fd6337a839aed709c0c0aa2ab1aa22fb
0velez0 <mariaivelez@gmail.com>
2018-05-08 16:10:56 -0700

    update rental model and controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,16 +1,28 @@
+require 'date'
+
 class RentalsController < ApplicationController
 
   def index
+
   end
 
   def new
+    # new rental has to have a customer id and a movie id???
+    @rental = Rental.new(params )
+    render json: rentals.as_json(only: [:id, :check_in, :check_out, :due_date])
+  end
+
+  def show
+
   end
 
   def create
   end
 
-  def destroy
-  end 
 
+private
+
+  def rental_params
+  return params.require(:rental).permit(:id, :check_in, :check_out, :due_date)
 
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,16 @@
 class Rental < ApplicationRecord
+
+  def check_in
+  end
+
+  def check_out
+  end
+
+  def due_date
+    # self.check_in + 7 days
+  end
+
+  def movie_available?
+  end
+
 end
