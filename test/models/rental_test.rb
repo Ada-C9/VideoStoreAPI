aedc7f299317867ee9d5d772a44c9b1479114a28
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-05-08 09:54:28 -0700

    Updated the relationship testing for rentals

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,90 +1,106 @@
 require "test_helper"
 
 describe Rental do
-  before do
-    @rental = Rental.new(
-      movie: movies(:LOTR),
-      customer: customers(:dan),
-      check_out: DateTime.now,
-      due_date: DateTime.now + 7
-    )
+  describe "relations" do
+    it "has a customer_id" do
+      rental = rentals(:one)
+      rental.must_respond_to :customer
+      rental.customer_id.must_equal customers(:dan).id
+    end
+
+    it "has a movie_id" do
+      rental = rentals(:two)
+      rental.must_respond_to :movie
+      rental.movie_id.must_equal movies(:LOTR).id
+    end
   end
 
-  it "can be created with all required fields" do
-    count_before = Rental.count
-    @rental.valid?.must_equal true
+  describe "validations" do
+    before do
+      @rental = Rental.new(
+        movie: movies(:LOTR),
+        customer: customers(:dan),
+        check_out: DateTime.now,
+        due_date: DateTime.now + 7
+      )
+    end
 
-    @rental.save
+    it "can be created with all required fields" do
+      count_before = Rental.count
+      @rental.valid?.must_equal true
 
-    Rental.last.movie.id.must_equal movies(:LOTR).id
-    Rental.count.must_equal count_before + 1
-  end
+      @rental.save
 
-  it "is invalid without a movie ID" do
-    count_before = Rental.count
-    @rental.movie_id = nil
-    @rental.valid?.must_equal false
+      Rental.last.movie.id.must_equal movies(:LOTR).id
+      Rental.count.must_equal count_before + 1
+    end
 
-    @rental.save
+    it "is invalid without a movie ID" do
+      count_before = Rental.count
+      @rental.movie_id = nil
+      @rental.valid?.must_equal false
 
-    @rental.errors.messages.must_include :movie_id
-    Rental.count.must_equal count_before
-  end
+      @rental.save
 
-  it "is invalid without a customer ID" do
-    count_before = Rental.count
-    @rental.customer_id = nil
-    @rental.valid?.must_equal false
+      @rental.errors.messages.must_include :movie_id
+      Rental.count.must_equal count_before
+    end
 
-    @rental.save
+    it "is invalid without a customer ID" do
+      count_before = Rental.count
+      @rental.customer_id = nil
+      @rental.valid?.must_equal false
 
-    @rental.errors.messages.must_include :customer_id
-    Rental.count.must_equal count_before
-  end
+      @rental.save
 
-  it "is invalid without a check out date" do
-    count_before = Rental.count
-    @rental.check_out = nil
-    @rental.valid?.must_equal false
+      @rental.errors.messages.must_include :customer_id
+      Rental.count.must_equal count_before
+    end
 
-    @rental.save
+    it "is invalid without a check out date" do
+      count_before = Rental.count
+      @rental.check_out = nil
+      @rental.valid?.must_equal false
 
-    @rental.errors.messages.must_include :check_out
-    Rental.count.must_equal count_before
-  end
+      @rental.save
 
-  it "is invalid without a due date" do
-    count_before = Rental.count
-    @rental.due_date = nil
-    @rental.valid?.must_equal false
+      @rental.errors.messages.must_include :check_out
+      Rental.count.must_equal count_before
+    end
 
-    @rental.save
+    it "is invalid without a due date" do
+      count_before = Rental.count
+      @rental.due_date = nil
+      @rental.valid?.must_equal false
 
-    @rental.errors.messages.must_include :due_date
-    Rental.count.must_equal count_before
-  end
+      @rental.save
 
-  it "is invalid if the due date is before the check out date" do
-    count_before = Rental.count
-    @rental.due_date = DateTime.now
-    @rental.check_out = DateTime.now + 1
-    @rental.valid?.must_equal false
+      @rental.errors.messages.must_include :due_date
+      Rental.count.must_equal count_before
+    end
 
-    @rental.save
+    it "is invalid if the due date is before the check out date" do
+      count_before = Rental.count
+      @rental.due_date = DateTime.now
+      @rental.check_out = DateTime.now + 1
+      @rental.valid?.must_equal false
 
-    @rental.errors.messages.must_include :due_date
-    Rental.count.must_equal count_before
-  end
+      @rental.save
 
-  it "is invalid if the check in date is before the check out date" do
-    count_before = Rental.count
-    @rental.check_in = DateTime.now
-    @rental.check_out = DateTime.now + 1
-    @rental.valid?.must_equal false
+      @rental.errors.messages.must_include :due_date
+      Rental.count.must_equal count_before
+    end
 
-    @rental.save
+    it "is invalid if the check in date is before the check out date" do
+      count_before = Rental.count
+      @rental.check_in = DateTime.now
+      @rental.check_out = DateTime.now + 1
+      @rental.valid?.must_equal false
 
-    @rental.errors.messages.must_include :check_in
-    Rental.count.must_equal count_before
+      @rental.save
+
+      @rental.errors.messages.must_include :check_in
+      Rental.count.must_equal count_before
+    end
   end
 end
