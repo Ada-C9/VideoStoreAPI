d138614f56244940d01c062a9531a97355565af0
Jamila <J.cornick21@gmail.com>
2018-05-07 22:29:49 -0700

    made available inventory show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
 
   def index
     movies = Movie.all
-      render(json: movies.as_json(only: [:id, :title, :release_date]), status: :ok)
+      render(json: movies.as_json(only: [:id, :title, :release_date, :inventory]), status: :ok)
   end
 
   def show
@@ -14,7 +14,7 @@ class MoviesController < ApplicationController
         }
       }, status: :not_found
     else
-      render json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory], status: :ok)
+      render json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory, :available_inventory], status: :ok)
     end
   end
 
