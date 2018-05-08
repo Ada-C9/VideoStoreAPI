a35bf240f3a31e49094d1bf58a03795540a0e741
Aruna <aruna.thungathurthi@gmail.com>
2018-05-07 22:20:01 -0700

    added customer controller test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,8 @@ class Customer < ApplicationRecord
   validates :registered_at, presence: true
   validates :postal_code, presence:true
   validates :phone, presence: true
+
+  def movies_checked_out_count
+    return 0
+  end
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -7,19 +7,41 @@ describe Customer do
   describe "relations" do
     it "has a list of rentals" do
       mary = customers(:mary)
-
       customer.rentals.each do |rental|
         rental.must_be_kind_of Rental
         rental.customer.must_be_instance_of Customer
         rental.customer.must_equal mary
       end
     end
-
   end
 
-  it "must be valid" do
+  describe "validations" do
 
+    it "must be valid" do
+      customer.valid?.must_equal true
+    end
 
+    it "must have validation for name" do
+      customer.name = nil
+      customer.valid?.must_equal false
+      customer.errors.messages.must_include :name
+    end
+    
+    it "must have validation for registered date" do
+      customer.registered_at = nil
+      customer.valid?.must_equal false
+      customer.errors.messages.must_include :registered_at
+    end
+    it "must have validation for phone" do
+      customer.phone = nil
+      customer.valid?.must_equal false
+      customer.errors.messages.must_include :phone
+    end
 
+  end
+  describe "method" do
+    it "returns the count of movies checked" do
+      customer.movies_checked_out_count.must_equal 0
+    end
   end
 end
