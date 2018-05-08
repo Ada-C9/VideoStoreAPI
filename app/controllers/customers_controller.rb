35ef9030776727d80528903a4e4b85208525efb4
Maja Graonic <m.graonic@gmail.com>
2018-05-08 15:15:56 -0700

    fixed more controller actions that failed smoke tests

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,7 +3,7 @@ class CustomersController < ApplicationController
 	def index
 		customers = Customer.all
 
-		render json: customers.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone]), status: :ok
+		render json: customers.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :movies_checked_out_count]), status: :ok
 	end
 
 	def show
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
   def index
     movies = Movie.all
 
-    render json: movies.as_json(only: [:id, :inventory, :title, :overview, :release_date]), status: :ok
+    render json: movies.as_json(only: [:id, :inventory, :title, :overview, :release_date, :available_inventory]), status: :ok
   end
 
   def show
@@ -17,7 +17,7 @@ class MoviesController < ApplicationController
         }
         }, status: :not_found
       else
-        render json: movie.as_json(only: [:overview, :release_date, :title, :inventory]), status: :ok
+        render json: movie.as_json(only: [:overview, :release_date, :title, :inventory, :available_inventory]), status: :ok
         # won't work unless rabl
         # render 'movies/show'
       end
