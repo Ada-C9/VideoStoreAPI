da78dcb782f374425bc3938f770c667fc260695c
Brenda Rios <riosbrenda@gmail.com>
2018-05-08 15:38:39 -0700

    Checkout action for the rentals controller.

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,13 +1,13 @@
 class RentalsController < ApplicationController
 
   def checkout
-    checkout_date = DateTime.now
+    # checkout_date = DateTime.now
 
     rental = Rental.new(rental_params)
-    customer = Customer.find_by(id: params[:customer_id])
-    movie = Movie.find_by(id: params[:movie_id])
+    # customer_id = Customer.find_by(id: rental_params[:customer_id])
+    # movie_id = Movie.find_by(id: rental_params[:movie_id])
 
-    rental.due_date = (checkout_date + 7)
+    # rental.due_date = (checkout_date + 7)
     if rental.save
       #success
       render json: rental_params, status: :ok
@@ -23,6 +23,6 @@ class RentalsController < ApplicationController
 
   private
   def rental_params
-    params.require(:rental).permit(:due_date, :customer_id, :movie_id)
+    params.permit(:due_date, :customer_id, :movie_id)
   end
 end
