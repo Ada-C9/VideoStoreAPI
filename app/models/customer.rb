0b8a18358671cfcac8c18b25ce2265e1d944643e
Kiera <kiera.thomasson@gmail.com>
2018-05-08 10:04:55 -0700

    created tests for customer model validations. Tests are passing.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :phone, length: { is: 10 }
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,39 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
-
-  it "must be valid" do
-    value(customer).must_be :valid?
+  before do
+    @customer_data = {
+      name: 'tester',
+      phone: '0000000000'
+    }
+    @customer = Customer.new(@customer_data)
   end
+
+  it "is valid with a name and phone number length of 10" do
+
+     @customer.must_be :valid?
+  end
+
+  it "is invalid with no name" do
+    @customer.name = nil
+
+    @customer.wont_be :valid?
+    @customer.errors.messages.must_include :name
+  end
+
+  it "is invalid with no phone number" do
+    @customer.phone = nil
+
+    @customer.wont_be :valid?
+    @customer.errors.messages.must_include :phone
+  end
+
+  it "is invalid with a phone number less than 10 char" do
+    @customer.phone = '3'
+    @customer.wont_be :valid?
+    @customer.errors.messages.must_include :phone
+  end
+
+
+
 end
