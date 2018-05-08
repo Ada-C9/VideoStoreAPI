006c6352f8348e440bde5136d641d517ec71ccb7
Cara <cararbrennan@gmail.com>
2018-05-08 12:31:14 -0700

    Rabl

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,26 +1,29 @@
 class MoviesController < ApplicationController
 
   def index
-    movies = Movie.all
+    @movies = Movie.all
       render(json: movies.as_json(only: [:id, :title, :release_date, :inventory]), status: :ok)
   end
 
   def show
-    movie = Movie.find_by(id: params[:id])
-    if movie.nil?
+    Rails.logger.level=:debug
+    @movie = Movie.find_by(id: params[:id])
+    if @movie.nil?
       render json: {
         errors: {
           id: ["No movie with ID #{params[:id]}"]
         }
       }, status: :not_found
     else
-      render json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory, :available_inventory], status: :ok)
+      puts "DPR about to do render"
+      # render json: @movie.as_json, status: :ok
+      render :show, status: :ok
     end
   end
 
   def create
-    movie = Movie.new(movie_params)
-    movie.save!
+    @movie = Movie.new(movie_params)
+    @movie.save!
     render json: {id: movie.id}, status: :ok
   end
 
---
+++ app/views/movies/show.rabl
@@ -0,0 +1,4 @@
+object @movie
+puts "DPR: inside the movie show rabl"
+puts "Movie is #{@movie.inspect}"
+attributes :id, :title, :overview , :release_date, :inventory, :available_inventory
