bd9dc0a1abae68d82c4a3ad8b72eabc2ccd6b2d5
Samantha Berk <sberk10@gmail.com>
2018-05-07 21:49:48 -0700

    Add controller tests to movies#index

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  # Maybe add a controller filter to get movies_url?
+
+  describe 'index' do
+    it "gets the movies route" do
+      get movies_url
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get movies_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array" do
+      get movies_url
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns a list of all movies" do
+      get movies_url
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+    it "returns movies with the required fields" do
+      keys = %w(id title release_date)
+      get movies_url
+      body = JSON.parse(response.body)
+      body.each do |movie
+        movie.keys.sort.must_equal keys
+      end
+    end
+
+  end
+
 end
