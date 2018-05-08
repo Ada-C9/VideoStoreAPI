4470c5e82ee98fa140f2e77f61f70a9b49fbd0c4
Hannah Cameron <hannahiscoding@gmail.com>
2018-05-07 21:00:20 -0700

    wrote customer#index controller tests - added in route and filled in method TDD

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  describe 'index' do
+    it 'successfully returns JSON containing all the customers' do
+      get customers_path
+
+      must_respond_with :success
+
+      response.header['Content-Type'].must_include 'json'
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+      body.length.must_equal Customer.count
+    end
+
+    it 'returns customers with the required fields' do
+      keys = %w(address city created_at id name phone postal_code registered_at state updated_at)
+
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal keys
+      end
+    end
+
+  end
 end
