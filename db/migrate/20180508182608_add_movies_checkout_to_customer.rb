ce86b8c20a7ab190b561e725d9b92d54da7da163
Ari <dominiquearia0@gmail.com>
2018-05-08 11:28:22 -0700

    added column to customer and movies

---
+++ db/migrate/20180508182608_add_movies_checkout_to_customer.rb
@@ -0,0 +1,7 @@
+class AddMoviesCheckoutToCustomer < ActiveRecord::Migration[5.1]
+  def change
+    add_column :customers, :movies_checked_out_count, :integer
+
+    add_column :movies, :available_inventory, :integer
+  end
+end
