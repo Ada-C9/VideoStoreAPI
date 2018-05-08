ea1454de2dc53734c1a09a203ca0948e9a314202
Aruna <aruna.thungathurthi@gmail.com>
2018-05-07 22:00:42 -0700

    added customer index method for controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,6 @@
 class CustomersController < ApplicationController
   def index
+    customers = Customer.all
+    render json: customers.as_json(only:[:id,:name,:registers_at,:postal_code,:phone,:methods=>[:movies_checked_out_count]])
   end
 end
