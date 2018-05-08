c1e61d7092dd482f3d37b6d769c9ffc08ab55a7e
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-05-08 10:01:33 -0700

    Generated migrations to add inventory and checkout values to customer and movie DB

---
+++ db/migrate/20180508165533_add_movie_checkout_column_to_customer.rb
@@ -0,0 +1,5 @@
+class AddMovieCheckoutColumnToCustomer < ActiveRecord::Migration[5.1]
+  def change
+    add_column :customers, :movies_checked_out_count, :integer
+  end
+end
---
+++ db/migrate/20180508165859_add_available_inventory_column_to_movie.rb
@@ -0,0 +1,5 @@
+class AddAvailableInventoryColumnToMovie < ActiveRecord::Migration[5.1]
+  def change
+    add_column :movies, :available_inventory, :integer
+  end
+end
