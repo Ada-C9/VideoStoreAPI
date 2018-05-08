6b8eae54ff1e946ea1edbe2a796ee43a231d645d
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-08 13:57:02 -0700

    Set up Movies Controller index tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,11 @@
 class MoviesController < ApplicationController
+
+  def index
+  movies = Movie.where(title: params[:title])
+
+  render json: movies.as_json(except: [:id,
+
+    :created_at, :updated_at], status: :ok)
+
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+    describe "index" do
+      it "returns an array of json" do
+        get movies_url
+        must_respond_with :success
+        response.header['Content-Type'].must_include 'json'
+        body = JSON.parse(response.body)
+        body.must_be_kind_of Array
+      end
+
+      it "returns movies with a name that matches the search" do
+        skip
+        get movies_url, params: {search: "Bob Esponja"}
+        body = JSON.parse(response.body)
+        body.each do |customer|
+          customer["name"].must_equal "Bob Esponja"
+        end
+      end
+
+      it "returns movies with exactly the required fields" do
+        skip
+        keys = %w(address city name phone postal_code registered_at state)
+        get movies_url, params: {search: "Bob Esponja"}
+        body = JSON.parse(response.body)
+        body.each do |customer|
+          customer.keys.sort.must_equal keys
+        end
+      end
+
+
+    end # index
 end
