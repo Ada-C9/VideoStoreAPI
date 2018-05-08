2e19a1074f27ae9a9c4f7f0b3ced04ee580681a6
Dikla <dikla.aharoni@gmail.com>
2018-05-08 10:46:32 -0700

    Added create method for rentals controller Updated the routes for create rental

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,19 @@
 class RentalsController < ApplicationController
+
+  def create
+    movie = Movie.find_by(id: params[:movie_id])
+    customer = Customer.find_by(id: params[:customer_id])
+    new_rental_data = {
+      customer_id: customer.id,
+      movie_id: movie.id
+    }
+    rental = Rental.new(new_rental_data)
+
+    if rental.save
+      render json: { errors: rental.errors.messages }, status: :bad_request
+    else
+      render :new
+    end
+  end
+
 end
