9a5a63f620740b972bad5ab6a35baa96e5aafd92
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-05-08 16:28:24 -0700

    Added Model Validations Tests for Rental Model

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,3 +1,6 @@
 class Rental < ApplicationRecord
-  
+  validates :checkout, presence: true
+  validates :due_date, presence: true
+  validates :customer_id, presence: true
+  validates :movie_id, presence: true
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,10 +1,11 @@
 require "test_helper"
+require 'pry'
 
 describe Rental do
   before do
     date = Date.today
     @rental_data = {
-      check_out: date,
+      checkout: date,
       due_date: date + 7,
       customer_id: Customer.first.id,
       movie_id: Movie.first.id
@@ -12,22 +13,51 @@ describe Rental do
   end
 
   it 'can be created with all valid information' do
-    skip
+    rental = Rental.new(@rental_data)
+    # binding.pry
+    rental.checkout.must_be_kind_of Date
+    rental.due_date.must_be_kind_of Date
+    rental.must_be :valid?
   end
 
   it 'cannot be created without customer_id' do
-    skip
+    rental = Rental.new(@rental_data)
+    rental.customer_id = nil
+
+    rental.wont_be :valid?
+    rental.errors.messages.must_include :customer_id
   end
 
   it 'cannot be created without movie_id' do
-    skip
+    rental = Rental.new(@rental_data)
+    rental.movie_id = nil
+
+    rental.wont_be :valid?
+    rental.errors.messages.must_include :movie_id
   end
 
   it 'cannot be created without check_out date' do
-    skip
+    rental = Rental.new(@rental_data)
+    rental.checkout = nil
+
+    rental.wont_be :valid?
+    rental.errors.messages.must_include :checkout
   end
 
   it 'cannot be created without due_date' do
-    skip
+    rental = Rental.new(@rental_data)
+    rental.due_date = nil
+
+    rental.wont_be :valid?
+    rental.errors.messages.must_include :due_date
+  end
+
+  it 'cannot be created with a checkout date not in the date format' do
+    rental = Rental.new(@rental_data)
+    rental.checkout = ""
+
+    rental.checkout.wont_be_kind_of Date
+    rental.wont_be :valid?
+    rental.errors.messages.must_include :checkout
   end
 end
