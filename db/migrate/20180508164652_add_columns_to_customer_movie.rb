a0cdbb8e7dc03c92f58a3e886a63aadf665571d1
Anne <annehwatson@gmail.com>
2018-05-08 09:50:03 -0700

    two new columns

---
+++ db/migrate/20180508164652_add_columns_to_customer_movie.rb
@@ -0,0 +1,7 @@
+class AddColumnsToCustomerMovie < ActiveRecord::Migration[5.1]
+  def change
+    add_column :customers, :movies_checked_out_count, :integer
+
+    add_column :movies, :available_inventory, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180507210818) do
+ActiveRecord::Schema.define(version: 20180508164652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180507210818) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "movies_checked_out_count"
   end
 
   create_table "customers_rentals", force: :cascade do |t|
@@ -43,6 +44,7 @@ ActiveRecord::Schema.define(version: 20180507210818) do
     t.integer "inventory"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "available_inventory"
   end
 
   create_table "rentals", force: :cascade do |t|
