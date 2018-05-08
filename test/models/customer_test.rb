6cdce7027b5bbe6ed8330e5c6433dacb20fb15ed
Ari <dominiquearia0@gmail.com>
2018-05-07 22:15:19 -0700

    added test for customer model

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,34 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  describe 'relations' do
+    it 'has a list of rentals' do
+      customer = customers(:kari)
+      customer.must_respond_to :rentals
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+      # customer.rentals.each do |rental|
+      #   rental.must_be_kind_of Rental
+      # end
+    end
+    it 'has a list of movies' do
+      customer = customers(:kari)
+      customer.must_respond_to :movies
+
+      # customer.rentals.each do |rental|
+      #   rental.must_respond_to Movie
+      # end
+    end
+  end
+
+  describe 'validations' do
+    it "must be valid" do
+      customer = customers(:kari)
+      customer.must_be :valid?
+    end
+    it "requires a name" do
+      customer = Customer.new
+      customer.valid?.must_equal false
+      customer.errors.messages.must_include :name
+    end
   end
 end
