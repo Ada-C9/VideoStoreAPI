9805f248ada0612527d12a1fab940a1c5aec8366
Karinna Iniguez <karinnainiguez@gmail.com>
2018-05-08 13:01:37 -0700

    rentals controller tests created.  Tests now passing with json rendered and statuses

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -7,10 +7,28 @@ class RentalsController < ApplicationController
       render json: {
         errors: rental.errors.messages
         }, status: :bad_request
+      end
+    end
+
+    def checkin
+      rental = Rental.find_by(id: params[:rental_id])
+
+      if rental
+        rental.checkin_date = DateTime.now
+        if rental.save
+          render json: rental.as_json, status: :ok
+        else
+          render json: {
+            errors: rental.errors.messages
+          }, status: :bad_request
+        end
+      else
+        render json: {
+          errors: {
+            rental: ["Rental not found"]
+          }
+        }, status: :not_found
+      end
+
     end
   end
-
-  def checkin
-
-  end
-end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -49,9 +49,49 @@ describe RentalsController do
   end
 
   describe "checkin" do
+
+    before do
+      rental_data = { movie_id: Movie.first.id, customer_id: Customer.first.id }
+      @rental = Rental.create_from_request(rental_data)
+      @rental.save
+    end
+
     it "should respond with success" do
+      post checkin_path, params: { rental_id: @rental.id }
+
+      must_respond_with :success
+    end
+
+    it "should respond with json of rental details" do
+      keys = %w(checkin_date checkout_date created_at customer_id due_date id movie_id updated_at)
+
+      post checkin_path, params: { rental_id: @rental.id }
+
+      response.header['Content-Type'].must_include 'json'
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+
+
+      body.keys.sort!.must_equal keys
 
     end
+
+    it "should respond with bad_request and error text if unable to checkin" do
+      rental_id = Rental.last.id + 1
+
+      post checkin_path, params: { rental_id: rental_id}
+
+      must_respond_with :not_found
+      response.header['Content-Type'].must_include 'json'
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include 'errors'
+
+    end
+
+
+
+
   end
 
 end
