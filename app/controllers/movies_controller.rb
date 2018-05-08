fc5610d5335ef5ab9294fed11f99e1f37684c862
Nora Peters <norapeters5@gmail.com>
2018-05-08 10:59:08 -0700

    adds index, show, and create methods to Movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,33 @@
 class MoviesController < ApplicationController
+
   def index
+    movies = Movie.all
+    render json: movies.as_json(only: [:id, :title, :release_date])
   end
 
+
   def show
+    movie = Movie.find_by(id: params[:id])
+    if movie
+      render json: movie.as_json(only: [:id, :inventory, :overview, :release_date, :title]), status: :ok
+    else
+      render json: { ok: false, errors: "Movie not found" }, status: :not_found
+    end
   end
 
   def create
+    movie = Movie.create(movie_params)
+    if movie.valid?
+      render json: { id: movie.id }, status: :ok
+    else
+      render json: { ok: false, errors: movie.errors },
+      status: :bad_request
+    end
   end
+end
+
+private
+
+def movie_params
+  return params.require(:movie).permit(:title, :overview, :release_date, :inventory)
 end
