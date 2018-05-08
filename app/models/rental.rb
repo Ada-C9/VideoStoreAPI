fcc917cc1f517431461402b5fd2df6cefb1643c8
kseastman <kat.eastman@gmail.com>
2018-05-08 10:24:33 -0700

    rental model testing, and movie, rental and customer relationships added.

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,22 @@
 class Rental < ApplicationRecord
+  belongs_to :movie
+  belongs_to :customer
+
+  def self.create_from_request(params_hash)
+    rental_data = {
+      checkout_date: DateTime.now,
+      due_date: DateTime.now + 7,
+      customer_id: params_hash[:customer_id],
+      movie_id: params_hash[:movie_id]
+    }
+
+    rental = self.new(rental_data)
+
+    # TODO: liked the way this worked with just return rental.save for boolean, figure it out if time?
+    if rental.save
+      return rental
+    else
+      return false
+    end
+  end
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,9 +1,80 @@
 require "test_helper"
 
 describe Rental do
-  let(:rental) { Rental.new }
+  before do
+    @movie = Movie.first
+    @customer = Customer.first
+  end
 
-  it "must be valid" do
-    value(rental).must_be :valid?
+  describe "Can be created" do
+    it "with valid data" do
+      old_rental_count = Rental.count
+
+      rental = Rental.new(movie: @movie, customer: @customer)
+
+      rental.must_be :valid?
+      rental.save
+
+      rental.must_be_kind_of Rental
+      Rental.count.must_equal old_rental_count + 1
+    end
+  end
+
+  describe "model methods" do
+    describe "rental#create_from_request" do
+      it "creates a rental" do
+        checkout_date = DateTime.now
+        due_date =  checkout_date + 7
+        old_rental_count = Rental.count
+
+        rental_data = {
+          customer_id: @customer.id,
+          movie_id: @movie.id
+        }
+
+        rental = Rental.create_from_request(rental_data)
+
+        result = (rental.due_date - due_date)
+
+        rental.must_be_kind_of Rental
+        Rental.count.must_equal old_rental_count + 1
+        result.must_be :<, 1
+      end
+
+      it "returns false if given invalid data" do
+        old_rental_count = Rental.count
+        rental_data = {
+          customer_id: nil,
+          movie_id: @movie.id
+        }
+
+        rental = Rental.create_from_request(rental_data)
+
+        rental.must_equal false
+        Rental.count.must_equal old_rental_count
+      end
+    end
+
+  end
+
+  describe "relationships" do
+    before do
+      @rental = Rental.create!(movie: @movie, customer: @customer)
+    end
+
+    it "ties rentals and customer" do
+      result = @rental.customer
+
+      result.must_be_kind_of Customer
+      result.id.must_equal @customer.id
+    end
+
+    it "ties rentals and movies" do
+      result = @rental.movie
+
+      result.must_be_kind_of Movie
+      result.id.must_equal @movie.id
+    end
+
   end
 end
