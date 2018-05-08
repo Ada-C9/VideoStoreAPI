3653d244b51b9c32806b325840054a615711b1f7
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-08 12:04:31 -0700

    added show controller action and tests for it

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,4 +3,18 @@ class CustomersController < ApplicationController
     customers = Customer.where(name: params[:search])
     render json: customers.as_json(except: [:created_at, :updated_at, :id], status: :ok)
   end
+
+  def show
+    customer = Customer.find_by(id: params[:id])
+
+    unless customer
+      render json: {errors: {
+        id: ["No customer with ID #{params[:id]}"]
+      }
+      }, status: :not_found
+    else
+
+      render json: customer.as_json(except: [:created_at, :updated_at], status: :ok)
+    end
+  end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -29,4 +29,31 @@ describe CustomersController do
 
 
   end # index
+
+  describe "show" do
+    it "can get a customer" do
+      keys = %w(address city id name phone postal_code registered_at state )
+      customer = Customer.first
+      get customer_path(customer.id)
+      must_respond_with :success
+
+      response.header['Content-Type'].must_include 'json'
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.keys.sort.must_equal keys
+      body['id'].must_equal customer.id
+    end
+
+    it "it should return not found and returns some error test when a customer does not exist" do
+      keys = %w(address city id name phone postal_code registered_at state )
+      customer_id = Customer.last.id + 1
+      get customer_path(customer_id)
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.must_include "errors"
+      body["errors"].must_include "id"
+
+    end
+  end
 end
