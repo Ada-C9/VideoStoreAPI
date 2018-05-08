021414e81a57108741d7a24b69b23e4abd5c3c1d
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-05-08 14:11:41 -0700

    Added a column in movies

--- db/migrate/20180508165906_change_typo_in_previous_migration.rb
+++ db/migrate/20180508165906_change_typo_in_previous_migration.rb
@@ -4,6 +4,6 @@ class ChangeTypoInPreviousMigration < ActiveRecord::Migration[5.1]
     remove_column :rentals, :customers_id
 
     add_reference :rentals, :movie, foreign_key: true
-    add_reference :rentals, :customer, foreign_key: true
+    add_reference :rentals, :customers, foreign_key: true
   end
 end
