750f104ddd4fa3ecca8c0d22a6a1627d3324396a
Abiaina <abinnet.a@gmail.com>
2018-05-08 13:53:28 -0700

    uncommented out model validations

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,7 +1,8 @@
 class Rental < ApplicationRecord
 
-  # validates :customer_id, :movie_id, presence: true
-  # validates :customer_id, :movie_id, numericality: true
+  validates :customer_id, :movie_id, presence: true
+
+  validates :customer_id, :movie_id, numericality: true
 
   belongs_to :customer, :movie
 end
