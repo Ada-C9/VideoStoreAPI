6e8df24ca4a5d3582d3b05c8fc3a7e7a71846b24
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-05-07 22:19:24 -0700

    Added some more validations for the models

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,37 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  describe "validations" do
+    it "Must have a title" do
+      movie = Movie.new(overview: "Great movie", release_date: Date.today, inventory: 4)
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+      movie.must_be_kind_of Movie
+      movie.wont_be :valid?
+    end
+
+    it "is valid with a title and inventory" do
+      movie = Movie.new(title: "El laberinto del Fauno", inventory: 5)
+
+      movie.must_be_kind_of Movie
+      movie.must_be :valid?
+
+    end
+
+    it "Must have an inventory" do
+      movie = Movie.new(overview: "Great movie", release_date: Date.today, title: "El laberinto del Fauno")
+
+      movie.must_be_kind_of Movie
+      movie.wont_be :valid?
+
+    end
+
+    it "must have a unique name" do
+      movie = Movie.first
+
+      other_movie = Movie.new(title: movie.title, inventory: 4)
+
+      other_movie.wont_be :valid?
+    end
+
   end
 end
