4dbfa70f75bd2ff3c84062a9f18e0918f204e31f
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-05-08 16:57:32 -0700

    Checkout in progress, params existing

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -4,6 +4,15 @@ class RentalsController < ApplicationController
   end
 
   def checkout
+    customer = Customer.find(params[:customer_id])
+    movie = Movie.find(params[:movie_id])
 
+    Rental.new(customer_id: customer, movie_id: movie)
   end
+
+  private
+  def checkout_params
+    params.permit(:customer_id, :movie_id)
+  end
+
 end
