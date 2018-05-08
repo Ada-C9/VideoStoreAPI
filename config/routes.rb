5d3fb0135dbd45397d56091a7807cdee00177ae0
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-05-08 12:07:36 -0700

    Added in Customer routes, Customer Controller actions, Customer Controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get '/movies', to: 'movies#index', as: 'movies'
   get '/movies/:id', to: 'movies#show', as: 'movie'
   post '/movies', to: 'movies#create', as: 'add_movie'
+
+  get '/customers', to: 'customers#index', as: 'customers'
 end
