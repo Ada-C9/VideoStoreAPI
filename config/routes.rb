0881dec9c53cf20437c9ccd39e09281c4bbdb5d4
Dikla <dikla.aharoni@gmail.com>
2018-05-08 10:06:12 -0700

    Added routes for rentals check-in and rentals check-out

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :customers, only: [:index, :show, :create]
   resources :movies, only: [:index, :show, :create]
+  post "/rentals", to:  "rentals#check-in", as: "check-in"
+  post "/rentals", to:  "rentals#check-out", as: "check-out"
   # get '/customers/zomg', to: "customers#zomg", as: 'zomg'
 end
