3511b519cfdd672e0900c2b1c5740ca16cb60817
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-05-08 15:14:11 -0700

    add checkin tests for the rentals controller

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -33,7 +33,7 @@ describe RentalsController do
       body.must_be_kind_of Hash
       body.must_include "id"
 
-    # Check that the ID matches
+      # Check that the ID matches
       Rental.find(body["id"]).movie_id.must_equal rental_data[:movie_id]
     end
 
@@ -51,6 +51,35 @@ describe RentalsController do
   end
 
   describe "checkin" do
+    #
+    # let(:checkin_data) {
+    #   {
+    #     movie_id: rental_one.movie_id,
+    #     customer_id: rental_one.customer_id,
+    #     returned?: true
+    #   }
+      # let(:bad_checkin_data) {
+      #   {
+      #     customer_id: rentals(:rental_two).customer_id,
+      #     returned?: true
+      #   }
+      # }
+
+    it "updates a rental when returned" do
+      proc {
+        post checkin_path(rental_one.id)
+      }.must_change "Rental.count", 0
+      must_respond_with :success
+      # updated_rental = Rental.find_by(id: rental_one.id)
+      rental_one.returned?.must_equal true
+    end
+
+    it "returns a bad request for a bad checkin" do
+      proc {
+        post checkin_path(3437829)
+      }.must_change "Rental.count", 0
+      must_respond_with :bad_request
+    end
 
   end
 
