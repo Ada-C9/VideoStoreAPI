cc3cdb2ff039cd8717e23f3a0cd4bf9cb58a2ad4
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-05-08 14:34:38 -0700

    Rabl working and all test passing

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,20 +1,20 @@
 class CustomersController < ApplicationController
 
   def index
-    customers = Customer.all
-      render(json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]), status: :ok)
+    @customers = Customer.all
+      render :index, status: :ok
   end
 
   def show
-    customer = Customer.find_by(id: params[:id])
-    if customer.nil?
+    @customer = Customer.find_by(id: params[:id])
+    if @customer.nil?
       render json: {
         errors: {
           id: ["No customer with ID #{params[:id]}"]
         }
       }, status: :not_found
     else
-      render json: customer.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone], status: :ok)
+      render :show, status: :ok
     end
   end
 
--- app/views/movies/show.rabl
+++ app/views/movies/show.rabl
@@ -1,4 +1,3 @@
 object @movie
-puts "DPR: inside the movie show rabl"
-puts "Movie is #{@movie.inspect}"
+
 attributes :id, :title, :overview , :release_date, :inventory, :available_inventory
