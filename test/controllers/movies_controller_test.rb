8f6fb2781e04c6f3478c717edda84489d2a86d61
Brittany Jones <brittany.r.jones@hotmail.com>
2018-05-08 13:01:01 -0700

    edit movies controller tests

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -2,18 +2,32 @@ require "test_helper"
 
 describe MoviesController do
   it "should get index" do
-    get movies_index_url
-    value(response).must_be :success?
+    get movies_url
+    must_respond_with :success
   end
 
-  it "should get show" do
-    get movies_show_url
-    value(response).must_be :success?
+  it "should still return success if there are no movies" do
+    Movie.destroy_all
+    Movie.count.must_equal 0
+    get movies_url
+    must_respond_with :success
   end
 
-  it "should get create" do
-    get movies_create_url
-    value(response).must_be :success?
+  it "should return json" do
+    get movies_url
+    response.header['Content-Type'].must_include "json"
   end
 
+  it "should return an array" do
+    get movies_url
+    body = JSON.parse(response.body)
+    body.must_be_kind_of Array
+  end
+
+  it "returns all of the movies" do
+    get movies_url
+
+    body = JSON.parse(response.body)
+    body.length.must_equal Movie.count
+  end
 end
