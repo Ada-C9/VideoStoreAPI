b8f512dcb5c2e6be70058b77dd5c573b09e8ffe9
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-05-07 17:29:26 -0700

    Added id

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,7 @@ class CustomersController < ApplicationController
 
   def index
     customers = Customer.all
-      render(json: customers.as_json(only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone]), status: :ok)
+      render(json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone]), status: :ok)
   end
 
   def show
