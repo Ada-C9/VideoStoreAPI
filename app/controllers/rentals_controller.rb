0b9ea38954482977bb079e21eb7229361e40bf57
Jamila <J.cornick21@gmail.com>
2018-05-08 16:22:20 -0700

    routes? rental controller

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,5 @@
+class RentalsController < ApplicationController
+  def method_name
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :movies, only: [:index, :show, :create]
-  resources :customers, only: [:index, :show]
+  resources :customers, only: [:index, :show] do
+    resources :rental, only: [:create], as: 'checkout'
+  end
+
+
 
 end
