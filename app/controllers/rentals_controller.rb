6e37985b3083a12394d955c7d4531cd98812876e
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-05-09 14:46:05 -0700

    Implemented first test in Checkout action for Rentals Controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,30 @@
 class RentalsController < ApplicationController
+
+  def checkout
+    movie_id = params[:movie_id]
+    customer_id = params[:customer_id]
+
+    date = Date.today
+    rental_params = {
+      checkout: nil,
+      due_date: nil,
+      customer_id: customer_id,
+      movie_id: movie_id
+    }
+
+    movie = Movie.find_by(id: movie_id)
+    customer = Customer.find_by(id: customer_id)
+
+    new_rental = Rental.new(rental_params)
+    new_rental[:checkout] = date
+    new_rental[:due_date] = date + 7
+
+    if new_rental.save
+      render json: { id: new_rental.id}, status: :ok
+      # movie_inventory = Movie.find_by(movie_id).inventory
+      # movie_inventory -= 1
+    else
+       render json: { errors: new_rental.errors.messages }, status: :bad_request
+    end
+  end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,21 +1,47 @@
 require "test_helper"
 
 describe RentalsController do
+  before do
+    movie = Movie.first
+    customer = Customer.first
+    @date = Date.today
+
+    @rental_data = {
+      checkout: nil,
+      due_date: nil,
+      customer_id: customer.id,
+      movie_id: movie.id
+    }
+
+  end
+
   describe 'checkout' do
     it 'is real route' do
-      skip
+      post checkout_path, params: @rental_data
+
+      must_respond_with :success
     end
 
     it 'can checkout a movie' do
-      skip
-    end
+      before_count = Rental.count
 
-    it 'assigns a checkout date' do
-      skip
-    end
+      # Act
+      post checkout_path, params: @rental_data
 
-    it 'changes the due date to 7 days from the checkout date' do
-      skip
+      assert_response :success
+      Rental.count.must_equal before_count + 1
+
+      body = JSON.parse(response.body)
+      body.must_include "id"
+      new_rental = Rental.find(body["id"])
+
+      new_rental.customer_id.must_equal @rental_data[:customer_id]
+
+      new_rental.movie_id.must_equal @rental_data[:movie_id]
+
+      new_rental.checkout.must_equal @date
+
+      new_rental.due_date.must_equal @date + 7
     end
 
     it 'changes inventory of movie with creation of a new checkout' do
@@ -27,21 +53,21 @@ describe RentalsController do
     end
   end
 
-  describe 'checkin' do
-    it 'is real route' do
-      skip
-    end
-
-    it 'can checkin a movie' do
-      skip
-    end
-
-    it 'changes the inventory of movie when it is checked in' do
-      skip
-    end
-
-    it 'throws an error if a movie is not checked out and you attempt a checkin' do
-      skip
-    end
-  end
+  # describe 'checkin' do
+  #   it 'is real route' do
+  #     skip
+  #   end
+  #
+  #   it 'can checkin a movie' do
+  #     skip
+  #   end
+  #
+  #   it 'changes the inventory of movie when it is checked in' do
+  #     skip
+  #   end
+  #
+  #   it 'throws an error if a movie is not checked out and you attempt a checkin' do
+  #     skip
+  #   end
+  # end
 end
