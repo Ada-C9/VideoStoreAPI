ece389e9ae48b6e9c96c07133fb8057bd6e578c4
Cara <cararbrennan@gmail.com>
2018-05-09 11:41:53 -0700

    pushing to change drivers

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -6,57 +6,73 @@ class RentalsController < ApplicationController
     if customer.nil?
       render json: {errors: {id: ["No such customer with ID #{check_params[:customer_id]}"]}}, status: :not_found
     elsif movie.nil?
-          render json: { errors: { id: ["No such movie with ID #{check_params[:movie_id]}"]}}, status: :not_found
+      render json: { errors: { id: ["No such movie with ID #{check_params[:movie_id]}"]}}, status: :not_found
     else
-            customer_id = customer.id
-            movie_id = movie.id
-            checkout_date = DateTime.now
-            due_date = checkout_date + 7
-            checkin_date = nil
+      customer_id = customer.id
+      movie_id = movie.id
+      checkout_date = DateTime.now
+      due_date = checkout_date + 7
+      checkin_date = nil
 
-            @rental = Rental.new(customer_id: customer_id, movie_id: movie_id, checkout_date: checkout_date, due_date: due_date, checkin_date: checkin_date)
+      @rental = Rental.new(customer_id: customer_id, movie_id: movie_id, checkout_date: checkout_date, due_date: due_date, checkin_date: checkin_date)
 
-            if @rental.save
-              if customer.movies_checked_out_count == nil
-                customer.movies_checked_out_count = 0
-                customer.save
-              end
-              Customer.find(@rental.customer_id).update_attributes movies_checked_out_count:  customer.movies_checked_out_count+1
+      if @rental.save
+        if customer.movies_checked_out_count == nil
+          customer.movies_checked_out_count = 0
+          customer.save
+        end
+        Customer.find(@rental.customer_id).update_attributes movies_checked_out_count:  customer.movies_checked_out_count+1
 
-              Movie.find(@rental.movie_id).update_attributes available_inventory: movie.available_inventory-1
-              render :checkout, status: :ok
-            else
-              render json: {errors: { unknown: ["Something went wrong"]}}, status: :bad_request
-            end
-    end
-
-  end
-
-
-
-    def checkin
-
-      rental = Rental.find(params[:rental_id])
-      rental.update_attributes checkin_date: DateTime.now
-
-      customer = Customer.find_by(rental.customer_id)
-      movie = Movie.find(rental.movie_id)
-    rental = Rental.find_by(id: check_params[:rental_id])
-    rental.update_attributes checkin_date: DateTime.now
-
-      if rental.save
-        Customer.find(rental.customer_id).update_attributes movies_checked_out_count:  customer.movies_checked_out_count-1
-
-        Movie.find(rental.movie_id).update_attributes available_inventory: movie.available_inventory+1
-
-        render json: { id: rental.id }, status: :ok
+        Movie.find(@rental.movie_id).update_attributes available_inventory: movie.available_inventory-1
+        render :checkout, status: :ok
       else
-        render json: { errors: rental.errors.messages }, status: :bad_request
-
+        render json: {errors: { unknown: ["Something went wrong"]}}, status: :bad_request
       end
+    end
+
+  end
+
+
+
+  def checkin
+    p "top of checkin"
+    (Rental.all).each do |rental|
+    puts  rental.id
+    puts "customerid"
+    puts rental.customer_id
+    puts "rental id"
+    puts      rental.movie_id
+      end
+    rental = Rental.find_by(id: check_params[:rental_id])
+    puts rental.inspect
+
+
+    rental.checkin_date = DateTime.now
+    rental.save
+    p "in checkin"
+    puts rental.inspect
+
+    movie_id = rental.movie_id
+    customer_id = rental.customer_id
+
+    customer = Customer.find_by(id: customer_id)
+    movie = Movie.find_by(id: movie_id)
+
+    if rental.save
+      p "in rental.save"
+      puts rental.inspect
+      Customer.find(rental.customer_id).update_attributes movies_checked_out_count:  customer.movies_checked_out_count-1
+
+      Movie.find(rental.movie_id).update_attributes available_inventory: movie.available_inventory+1
+
+      render json: { id: rental.id }, status: :ok
+    else
+      render json: { errors: rental.errors.messages }, status: :bad_request
 
     end
+
   end
+
 
   private
   def check_params
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,36 +5,6 @@ class Movie < ApplicationRecord
 
    # possible logic to decrement available_inventory
 
-<<<<<<< HEAD
-             available_inventory -= 1
-             
-           end
-=======
-   # returns true if movie is available for checkout
-   # returns false if movie isn't available for checkout
-   def self.available_inventory?
-     self.available_inventory >= 1
-   end
 
-  def self.reduce_available_inventory
-    self.available_inventory - 1
-  end
->>>>>>> 9b21c8ccf1fee5b0636e2d032e1594ac14edcee0
-
-  def self.available_inventory?
-    unless self.available_inventory == 0
-      rental_history = self.rentals
-
-      rental_history.each do |item|
-        if item.checkout_date.nil && item.rental.checkin_date == nil
-          available_inventory -= 1
-        end
-      end
-
-    end
-
-    return "We do not have that title available now."
-
-   end
 
 end
