89dd2232f2524d82a9f45d9cf2754361bb8e6051
knockknockhusthere <angela.huxia@gmail.com>
2018-05-09 10:29:33 -0700

    updated rentals checkout method, now working

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,21 +2,20 @@ require 'date'
 
 class RentalsController < ApplicationController
 
-  # def create
-  #   @rental = Rental.new(params)
-  #
-  #   @rental.check_out = Date.today
-  #   @rental.due_date = Date.today + 7
-  # end
-
   def check_out
     # new rental has to have a customer id and a movie id???
     # when you check_out a rental, you create a rental
     # create a new check in date?
-    @rental = Rental.new(params)
+    @rental = Rental.new(rental_params)
     #check movies availability - in a movie method?
     @rental.check_out = Date.today
     @rental.due_date = Date.today + 7
+
+    if @rental.save
+      render json: { id: @rental.id }, status: :ok
+    else
+      render json: { errors: @rental.errors.messages }, status: :bad_request
+    end
     # render json: @rental.as_json(only: [:id, :check_in, :check_out, :due_date])
 
   end
@@ -31,6 +30,6 @@ class RentalsController < ApplicationController
   private
 
   def rental_params
-    return params.require(:rental).permit(:id, :check_in, :check_out, :due_date)
+    return params.permit(:id, :check_in, :check_out, :due_date, :movie_id, :customer_id)
   end
 end
