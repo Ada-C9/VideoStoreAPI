0a361f1a5fa920b45b0599032707cc73b6f913b4
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-05-09 09:37:29 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   resources :movies, only: [:index, :show, :create]
   resources :customers, only: [:index]
-  
+
+  post 'rentals/check-out', to: 'rentals#check_out', as: 'checkout'
+
+  post 'rentals/check-in', to: 'rentals#check_in', as: 'checkin'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
