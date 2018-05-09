dfc3908ee1e9731ce39c87fb9e97448166771d98
kseastman <kat.eastman@gmail.com>
2018-05-09 16:26:24 -0700

    temporary unsatisfactory solution in customers_controller for params query.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,11 +1,15 @@
 class CustomersController < ApplicationController
   def index
-    @customers = Customer.all
+    if params[:sort]
+      @customers = Customer.order(:name)
+    else
+      @customers = Customer.all
+    end
     if @customers.empty?
       render json: {errors: {
         customer: ["No customers were found"]
       }
-    }, status: :not_found
+      }, status: :not_found
 
     else
       render 'index.json'
