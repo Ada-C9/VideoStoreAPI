8ec5286de2d8929a074c6c9e1f0f3702527a989d
Abiaina <abinnet.a@gmail.com>
2018-05-09 15:56:45 -0700

    added column to customers tracking movie rental count, reseeded data, with new column set to 0

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 JSON.parse(File.read('db/seeds/customers.json')).each do |customer|
-  Customer.create!(customer)
+  new_customer = Customer.create!(customer)
+  new_customer.movies_checked_out_count = 0
+  new_customer.save
 end
 
 JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
   new_movie = Movie.create!(movie)
   new_movie.available_inventory = new_movie.inventory
-  new_movie.save  
+  new_movie.save
 end
