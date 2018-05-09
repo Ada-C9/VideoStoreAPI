304b9af6aeed99a5ef8391aabe8d79569c109982
Ari <dominiquearia0@gmail.com>
2018-05-09 15:30:20 -0700

    added helper methods to rental controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,18 +2,12 @@ class RentalsController < ApplicationController
 
   def checkout
     rental = Rental.new(rental_params)
-    rental.checkout_date= Date.today
-    rental.due_date= (rental.checkout_date + 7)
+    Rental.rental_date(rental)
 
     if rental.save
-      new_inventory = rental.movie.available_inventory -= 1
-      customer_movie_count = rental.customer.movies_checked_out_count += 1
-      #success
-      rental.update_attribute(:checked_out, true)
-      rental.movie.update_attribute(:available_inventory, new_inventory)
-      rental.customer.update_attribute(:movies_checked_out_count, customer_movie_count)
-      
+      Rental.build_rental(rental)
       render json: rental_params, status: :ok
+
     else
       #failure
       render json: {errors: rental.errors.messages }, status: :bad_request
@@ -30,13 +24,7 @@ class RentalsController < ApplicationController
           }
           }, status: :not_found
     else
-      new_inventory = rental.movie.available_inventory += 1
-      customer_movie_count = rental.customer.movies_checked_out_count -= 1
-
-      rental.update_attribute(:checked_out, false)
-      rental.movie.update_attribute(:available_inventory, new_inventory)
-      rental.customer.update_attribute(:movies_checked_out_count, customer_movie_count)
-
+      Rental.build_return(rental)
       render(json: rental.as_json(only: [:customer_id, :movie_id]), status: :ok)
     end
   end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,4 +3,29 @@ class Rental < ApplicationRecord
   belongs_to :movie
 
   validates :due_date, presence: true
+
+  def self.rental_date(rental)
+    rental.checkout_date= Date.today
+    rental.due_date= (rental.checkout_date + 7)
+  end
+
+  def self.build_rental(rental)
+    new_inventory = rental.movie.available_inventory -= 1
+    customer_movie_count = rental.customer.movies_checked_out_count += 1
+
+    rental.update_attribute(:checked_out, true)
+    rental.movie.update_attribute(:available_inventory, new_inventory)
+    rental.customer.update_attribute(:movies_checked_out_count, customer_movie_count)
+  end
+
+  def self.build_return(rental)
+    new_inventory = rental.movie.available_inventory += 1
+    customer_movie_count = rental.customer.movies_checked_out_count -= 1
+
+    rental.update_attribute(:checked_out, false)
+    rental.movie.update_attribute(:available_inventory, new_inventory)
+    rental.customer.update_attribute(:movies_checked_out_count, customer_movie_count)  
+  end
+
+
 end
