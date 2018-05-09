d4bb46c8a554e682bf21e96c6f4f370cd3cf822a
Anne <annehwatson@gmail.com>
2018-05-08 18:52:58 -0700

    rental model test

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,11 +1,22 @@
 require "test_helper"
 
 describe Rental do
+
   movie = Movie.first
   customer = Customer.first
   let(:rental) { Rental.new(movie_id: movie.id, customer_id: customer.id) }
-  
+
   it "must be valid" do
     value(rental).must_be :valid?
+  end
+
+  describe "relationship" do
+    it "belongs to movie" do
+      rental.must_respond_to :movie
+    end
+
+    it "belongs to customer" do
+      rental.must_respond_to :customer
+    end
   end
 end
