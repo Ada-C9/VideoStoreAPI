f0c50876ca1792c05e6d4b3aa99deb0b4bfd668e
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-05-09 15:25:48 -0700

    Tests passing

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,17 +1,5 @@
 require "test_helper"
 
 describe Rental do
-  let(:rental) { Rental.new }
-
-  it "must be valid with all fields" do
-    value(rental).must_be :valid?
-
-end
-    it "wont be valid if there is no due date once checked out" do
-      let(:rental) { Rental.new(checkout_date: DateTime.now, due_date: " ") }
-      value(rental).wont_be :valid?
-    end
-
-
 
 end
