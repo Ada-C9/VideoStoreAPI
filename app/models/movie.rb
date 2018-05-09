d4047fd6950660f9e5d6503617874b58c0a97cf0
Jamila <J.cornick21@gmail.com>
2018-05-08 23:13:05 -0700

    tried writing rentals model validation some tests for that and logic for movies model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,27 @@ class Movie < ApplicationRecord
    validates :title, presence: true
    has_many :rentals
    has_many :customers, through: :rentals
+
+   # possible logic to decrement available_inventory
+   def self.available_inventory?
+     unless self.available_inventory == 0
+       rented_copies = self.rental.where(id: self.id)
+
+         rented_copies.each do |copy|
+           if copy.checkout_date && copy.rental.checkin_date == nil
+
+             available_inventory -= 1
+
+           end
+
+         end
+       else
+
+         return "We do not have that title available now."
+
+     end
+
+
+   end
+
 end
