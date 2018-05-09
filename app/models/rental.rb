d9a48ddfec3ecfbddd18ea09092213f959a2ba0d
Jamila <J.cornick21@gmail.com>
2018-05-08 22:28:32 -0700

    rental model possible validation and tests

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,10 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :movie
+  validates :due_date, presence: true, if: :checkout_date?
+ 
+  def checkout_date?
+    checkout_date == DateTime.now
+  end
+
 end
