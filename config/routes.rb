1fbcdbfb379285bf6cc1a3d95175fc448de73e44
Lily Sky <lilyxsky@gmail.com>
2018-05-09 16:26:13 -0700

    post checkout route updated

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   #movies_controller
     resources :movies, only:[:index, :show, :create]
 
-  post 'rentals/check-out/:movie_id/:customer_id', to: 'rentals#create', as: 'rental'
+  post 'rentals/check-out', to: 'rentals#create', as: 'rental'
 
   post 'rentals/check-in/:rental_id', to: 'rentals#update', as: 'rental_update'
 
