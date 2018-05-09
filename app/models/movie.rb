5197527ff0de6d4815954c7daf67cc48a4978b56
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-09 11:56:07 -0700

    Write available_inventory method for Movie model to use in show RABL page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,15 @@
 class Movie < ApplicationRecord
-  validates :title, presence: true 
+  validates :title, presence: true
   validates :inventory, presence: true
   has_many :rentals
+
+  def get_available_inventory
+    checked_out_count = 0
+    self.rentals.each do |rental|
+      if rental.return_date.nil?
+        checked_out_count += 1
+      end
+    end
+    return self.inventory - checked_out_count
+  end
 end
