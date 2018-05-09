2b62a263292d1bd4208e948b0af344dc979aa725
knockknockhusthere <angela.huxia@gmail.com>
2018-05-09 09:31:24 -0700

    checkin and checkout routes

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,29 +2,35 @@ require 'date'
 
 class RentalsController < ApplicationController
 
-  def index
+  # def create
+  #   @rental = Rental.new(params)
+  #
+  #   @rental.check_out = Date.today
+  #   @rental.due_date = Date.today + 7
+  # end
 
-  end
-
-  def new
+  def check_out
     # new rental has to have a customer id and a movie id???
+    # when you check_out a rental, you create a rental
+    # create a new check in date?
     @rental = Rental.new(params)
+    #check movies availability - in a movie method?
+    @rental.check_out = Date.today
+    @rental.due_date = Date.today + 7
+    # render json: @rental.as_json(only: [:id, :check_in, :check_out, :due_date])
 
-    render json: @rental.as_json(only: [:id, :check_in, :check_out, :due_date])
   end
 
   def check_in
-      # when you check_out a rental, you create a rental
+    # when you check_in a rental, you update a rental
+    @rental = Rental.find_by(params)
+
+    @rental.check_in = Date.today
   end
 
-  def check_out
-      # when you check_in a rental, you update a rental
-  end
+  private
 
-
-# private
-#
-#   def rental_params
-#   return params.require(:rental).permit(:id, :check_in, :check_out, :due_date)
-
+  def rental_params
+    return params.require(:rental).permit(:id, :check_in, :check_out, :due_date)
+  end  
 end
