2bc4e90170cab9d4666a522016fbd76f49dba574
Mary Lamkin <mmlamkin@gmail.com>
2018-05-09 14:49:27 -0700

    removes unneeded instance variables in controllers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,7 @@
 class CustomersController < ApplicationController
   def index
-    @customers = Customer.all
 
-    # render json: @customers.as_json(only: [ :id, :name, :phone, :postal_code, :registered_at, :movies_checked_out_count])
+    @customers = Customer.all
+    
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,32 +1,34 @@
 class MoviesController < ApplicationController
   def index
+
     @movies = Movie.all
 
-    render json: @movies.as_json(only: [ :id, :title, :release_date])
   end
 
   def show
+
     @movie = Movie.find_by(id: params[:id])
+
     if @movie.nil?
-    #   render json: @movie.as_json(only: [:title, :overview, :release_date, :inventory, :available_inventory])
-    # else
       render json: {ok: false, :errors => "Movie not found"}, status: :not_found
     end
+
   end
 
   def create
-    @movie = Movie.new(movie_params)
 
-    if @movie.save
-      render json: {id: @movie.id}, status: :ok
+    movie = Movie.new(movie_params)
+
+    if movie.save
+      render json: {id: movie.id}, status: :ok
     else
-      render json: {ok: false, errors: @movie.errors}, status: :bad_request
+      render json: {ok: false, errors: movie.errors}, status: :bad_request
     end
   end
 
   private
 
-def movie_params
-  return params.permit(:title, :overview, :release_date , :inventory)
-end
+  def movie_params
+    return params.permit(:title, :overview, :release_date , :inventory)
+  end
 end
