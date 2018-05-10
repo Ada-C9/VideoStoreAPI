85e308706e7b04b4e175255ae9c54613a7360f93
0velez0 <mariaivelez@gmail.com>
2018-05-10 00:04:19 -0700

    update rentals_controller to include available_inventory methods called on movie

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,15 +3,15 @@ require 'date'
 class RentalsController < ApplicationController
 
   def check_out
-    # new rental has to have a customer id and a movie id???
-    # when you check_out a rental, you create a rental
-    # create a new check in date?
     rental = Rental.new(rental_params)
     #check movies availability - in a movie method?
     rental.check_out = Date.today
     rental.due_date = Date.today + 7
 
     if rental.save
+      movie = Movie.find_by(id: rental.movie_id)
+      movie.dec_avail_inventory
+
       render json: { id: rental.id }, status: :ok
     else
       render json: { errors: rental.errors.messages }, status: :bad_request
@@ -20,21 +20,23 @@ class RentalsController < ApplicationController
 
   def check_in
     rental = Rental.find_by(customer_id: params[:customer_id], movie_id: params[:movie_id])
-    if rental.nil?
-    render json: {
-      "errors": {
-        "id": ["No rental with #{params[:id]}"]
-      }
-      }, status: :not_found
-      else
-        # logic for increasing available_inventory
-        render(json: rental.as_json(only: [:customer_id, :movie_id]), status: :ok)
-      end
-    end
+    if rental
+      movie = Movie.find_by(id: rental.movie_id)
+      movie.inc_avail_inventory
 
-    private
-
-    def rental_params
-      params.permit(:id, :check_in, :check_out, :due_date, :movie_id, :customer_id)
+      render(json: rental.as_json(only: [:customer_id, :movie_id]), status: :ok)
+    else
+      render json: {
+        "errors": {
+          "id": ["No rental with #{params[:id]}"]
+        }
+        }, status: :not_found
     end
   end
+  
+private
+
+  def rental_params
+    params.permit(:id, :check_in, :check_out, :due_date, :movie_id, :customer_id)
+  end
+end
