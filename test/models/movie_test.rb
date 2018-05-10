36fff26a3d251c1c50303fcdb14e110c4c204eb2
0velez0 <mariaivelez@gmail.com>
2018-05-10 00:01:41 -0700

    add movie model tests for available_inventory, decreasing and increasing inventory of movies

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -43,4 +43,43 @@ describe Movie do
       result.must_equal false
     end
   end
+
+  describe 'describe available inventory methods' do
+
+    # the next test uses the yml 'gijane' which has different inventory compared to available_inventory
+    it 'sets the available_inventory of a movie equal to the inventory' do
+      movie = movies(:gijane)
+
+      movie.set_avail_inventory
+
+      movie.available_inventory.must_equal 3
+    end
+
+    # the next two tests uses the yml 'purple' which has available_inventory set to nil as a default
+    it 'decreases available inventory of a movie by 1' do
+      movie = movies(:purple)
+      movie.set_avail_inventory
+      before_count = movie.available_inventory
+
+      movie.dec_avail_inventory
+
+      after_count = movie.available_inventory
+
+      after_count.must_equal before_count - 1
+
+    end
+
+    it 'increases available inventory of a movie by 1' do
+      movie = movies(:purple)
+      movie.set_avail_inventory
+      before_count = movie.available_inventory
+
+      movie.inc_avail_inventory
+
+      after_count = movie.available_inventory
+
+      after_count.must_equal before_count + 1
+
+    end
+  end
 end
