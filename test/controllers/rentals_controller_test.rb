15d66aba1eb151f9c78bd917e901f57064f79075
0velez0 <mariaivelez@gmail.com>
2018-05-09 23:57:52 -0700

    add two tests: won't create a rental when movie_id and customer_id DNE

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,9 +1,7 @@
 require "test_helper"
-
+require 'pry'
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
   describe 'check_out' do
     before do
       @customer = Customer.first
@@ -20,14 +18,22 @@ describe RentalsController do
       Rental.count.must_equal before_rental_count + 1
 
       Rental.last.movie_id.must_equal @params[:movie_id]
+
       Rental.last.customer_id.must_equal @params[:customer_id]
     end
 
     it "won't create a rental when movie_id DNE" do
-      #Maria
+      params = { customer_id: @customer.id, movie_id: nil }
+
+      post check_out_path, params: params
+      assert_response :bad_request
     end
 
-    it "won't create a rental when customer_id DNE"
-    #Maria
+    it "won't create a rental when customer_id DNE" do
+
+      params = { customer_id: nil, movie_id: @movie.id }
+      post check_out_path, params: params
+      assert_response :bad_request
     end
+  end
 end
