23f706e44c0e0d1ea5498324f8d588ca23a33e09
Karinna Iniguez <karinnainiguez@gmail.com>
2018-05-09 17:27:16 -0700

    optional params accounted for in customers controller.  Controller tests passing

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,71 @@
 class CustomersController < ApplicationController
   def index
-    if params[:sort]
-      @customers = Customer.order(:name)
-    else
+
+    queries = {
+      sort: params["sort"],
+      n: params["n"],
+      p: params["p"]
+    }
+
+
+    if queries[:sort]
       @customers = Customer.all
+      @customers = @customers.order(queries[:sort])
+
+      if queries[:p]
+        if queries[:n]
+          # IF BOTH P AND N
+          start_index = ( queries[:p].to_i - 1 ) * queries[:n].to_i
+          ending_index = start_index + queries[:n].to_i
+          @customers = @customers[start_index...ending_index]
+
+        else
+          # IF ONLY P
+          start_index = (queries[:p].to_i - 1) * 10
+          ending_index = start_index + 10
+
+          @customers = @customers[start_index...ending_index]
+
+        end
+      elsif queries[:n]
+        # IF ONLY N
+        start_index = 0
+        ending_index = queries[:n].to_i
+
+        @customers = @customers[start_index...ending_index]
+
+      end
+
+    else
+
+      @customers = Customer.all
+
+      if queries[:p]
+        if queries[:n]
+          # IF BOTH P AND N WITHOUT SORT
+          start_index = ( queries[:p].to_i - 1 ) * queries[:n].to_i
+          ending_index = start_index + queries[:n].to_i
+          @customers = @customers[start_index...ending_index]
+
+        else
+          # IF ONLY P WITHOUT SORT
+          start_index = (queries[:p].to_i - 1) * 10
+          ending_index = start_index + 10
+
+          @customers = @customers[start_index...ending_index]
+
+        end
+      elsif queries[:n]
+        # IF ONLY N WITHOUT SORT
+        start_index = 0
+        ending_index = queries[:n].to_i
+
+        @customers = @customers[start_index...ending_index]
+
+      end
+
     end
+
     if @customers.empty?
       render json: {errors: {
         customer: ["No customers were found"]
