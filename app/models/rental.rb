a7b4b03cb7278b22307a4e7a46392facf390b2a4
Jamila <J.cornick21@gmail.com>
2018-05-10 09:56:48 -0700

    working on rental controller tests for check_in method

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,9 +2,9 @@ class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :movie
   validates :due_date, presence: true, if: :checkout_date?
- 
+
   def checkout_date?
-    checkout_date == DateTime.now
+    !checkout_date.nil?
   end
 
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -16,4 +16,26 @@ describe RentalsController do
 
   end
 
+  describe "checkin" do
+    it "responds with success" do
+      post check_in_url
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      post check_in_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "update checkin field on valid rental" do
+
+    end
+
+    it "does not checkin an invalid rental" do
+
+    end
+  end
+
+
+
 end
