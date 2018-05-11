926d116b17e05f86ea0a8d84296fadc368403980
Nora Peters <norapeters5@gmail.com>
2018-05-11 15:15:12 -0700

    adds rentals#overdue

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -7,7 +7,7 @@ class RentalsController < ApplicationController
       render json: { ok: false }, status: :bad_request
     else
       if movie.available_inventory > 0
-        rental = Rental.create(movie_id: params[:rental][:movie_id], customer_id: params[:rental][:customer_id], returned?: false)
+        rental = Rental.create(movie_id: params[:rental][:movie_id], customer_id: params[:rental][:customer_id], returned?: false, checkout_date: Date.today)
         if rental
           render json: rental.as_json(), status: :ok
         else
@@ -29,6 +29,22 @@ class RentalsController < ApplicationController
       rental.update(returned?: true)
       render json: rental.as_json(), status: :ok
     end
+  end
+
+  def overdue
+    rentals = Rental.get_overdue
+    # if params[:sort] == "title"
+    #   rentals.order(:movie_id.title).paginate(page: params[:p], per_page: params[:n])
+    # elsif params[:sort] == "name"
+    #   rentals.order(:name).paginate(page: params[:p], per_page: params[:n])
+    # elsif params[:sort] == "checkout_date"
+    #   rentals.order(:checkout_date).paginate(page: params[:p], per_page: params[:n])
+    # elsif params[:sort] == "due_date"
+    #   rentals.order(:checkout_date).paginate(page: params[:p], per_page: params[:n])
+    # else
+    #   rentals
+    # end
+    render json: rentals.as_json(), status: :ok
   end
 
   private
