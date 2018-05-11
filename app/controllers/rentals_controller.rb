ddd08d17e75d2bf2ba468f29120dbbe4f6ee9648
Lily Sky <lilyxsky@gmail.com>
2018-05-11 13:02:33 -0700

    rental code adjustments - invalid create tests need attention

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,15 +3,25 @@ require 'date'
 class RentalsController < ApplicationController
 
   def create
-    movie_id_params = params[:movie_id]
-    customer_id_params = params[:customer_id]
+    if params[:movie_id].nil? || params[:customer_id].nil?
+      # this renders multiple times so I commented it out
+      # I'm unsure why its rendering multiple times
+      # I set this up to send a bad request/not_found for nil values for ids in params and also test if movie can be rented
 
-    chosen_movie = Movie.find_by(id: movie_id_params)
+      render json: {ok: false }, status: :not_found
+    else
+      found_movie = Movie.find_by(id: params[:movie_id])
+      chosen_movie = Movie.rentable_movie?(found_movie.id)
+    end
+
+    # movie_id_params = params[:movie_id]
+    # customer_id_params = params[:customer_id]
+    # chosen_movie = Movie.find_by(id: movie_id_params)
     today = Date.today
 
     rental_data = {
-      movie_id: movie_id_params,
-      customer_id: customer_id_params,
+      movie_id: chosen_movie.id,
+      customer_id: params[:customer_id],
       check_out_date: today.to_s,
       due_date: (today + 7).to_s
     }
@@ -21,15 +31,7 @@ class RentalsController < ApplicationController
     # I added this check to confirm that the movie is available to save before we make a new rental
     # called the movie.rentable_movie? to
 
-    if movie_id_params.nil? || customer_id_params.nil?
-      # this renders multiple times so I commented it out
-      # I'm unsure why its rendering multiple times
-      # I set this up to send a bad request/not_found for nil values for ids in params and also test if movie can be rented
 
-      # render json: {ok: false }, status: :not_found
-    else
-      chosen_movie = Movie.rentable_movie?(chosen_movie.id)
-    end
 
     if chosen_movie && rental.save
       Movie.decrement(chosen_movie)
