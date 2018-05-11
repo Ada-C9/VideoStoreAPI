f2e34246373e35b266b597a7d01fc809c9f3fef2
Maja Graonic <m.graonic@gmail.com>
2018-05-11 14:48:24 -0700

    updated rental actions in controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -14,7 +14,6 @@ class RentalsController < ApplicationController
     due_date = checkout_date + 7
 
     # initialize a rental
-
     rental = Rental.new(customer_id: customer_id, movie_id: movie_id, checkout_date: checkout_date, due_date: due_date)
     # head :bad_request unless movie && customer
 
@@ -42,30 +41,22 @@ class RentalsController < ApplicationController
   end
 
   def checkin
-    # # take in query params
-    # movie_id = params[:movie_id]
-    # customer_id = params[:customer_id]
-    # 
-    # # find rental with corresponding attributes
-    # rental = Rental.find_by(movie_id: params[:movie_id], customer_id: params[:customer_id])
-    #
-    # # set rental due date to nil
-    # rental.due_date = nil
+    movie_id = params[:movie_id]
+    customer_id = params[:customer_id]
 
-    movie = Movie.find_by(id: params[:movie_id])
-    customer = Customer.find_by(id: params[:customer_id])
-    head :not_found unless movie && customer
+    rental = Rental.where(movie_id: movie.id, customer_id: customer.id)
 
-    customer.movies << movie
+    # make sure it's a valid rental???
 
-    if customer.save
-      rental = Rental.where(movie_id: movie.id, customer_id: customer.id).take
+    if rental && rental.due_date != nil
 
-      if rental.due_date.class == Date
-        rental.due_date = nil
+      rental.due_date = nil
+      rental.save
 
-        render json: rental.as_json(only: [:id, :checkout_date, :due_date]), status: :ok
-      end
+      rental.customer.movies_checked_out_count -= 1
+      rental.customer.save
+
+      render json: rental.as_json(only: [:id, :checkout_date, :due_date]), status: :ok
     else
       render json: { errors: rental.errors.messages }, status: :bad_request
     end
