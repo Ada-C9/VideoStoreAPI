efefc9ea1070693c6d5dda83c8ffdc569103fb45
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-05-11 14:24:03 -0700

    Currently only two tests not passing, working on those tests now

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -24,7 +24,7 @@ class RentalsController < ApplicationController
       } , status: :bad_request
       return
     end
-    
+
     new_rental = Rental.new(rental_params)
     new_rental[:checkout] = date
     new_rental[:due_date] = date + 7
@@ -35,5 +35,31 @@ class RentalsController < ApplicationController
     else
       render json: { errors: new_rental.errors.messages }, status: :bad_request
     end
+  end
+
+  def checkin
+    rental = Rental.find_by(params[:id])
+
+    if rental[:checkout].nil? || rental[:due_date].nil?
+      render json: {
+        errors: {
+          checkout: ["Movie has not been checked out."],
+        due_date: ["Movie has not been checked out."]
+        }
+      }, status: :bad_request
+      return
+    end
+    movie = Movie.find_by(rental.movie_id)
+    movie[:inventory] += 1
+    if movie.save
+      render json: { id: movie.id }, status: :ok
+    else
+      render json: { errors: movie.errors.messages }, status: :bad_request
+    end
+  end
+
+  private
+  def rental_params
+    return params.permit(:checkout, :due_date, :customer_id, :movie_id)
   end
 end
